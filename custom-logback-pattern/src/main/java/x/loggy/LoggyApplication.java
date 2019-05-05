0b80123f792ee5e7e0f5dfe40422c08bfd9622db
Brian Oxley <boxley@thoughtworks.com>
2019-05-05 15:57:12 -0500

    With markers

--- custom-logback-pattern/src/main/java/x/loggy/LoggyApplication.java
+++ custom-logback-pattern/src/main/java/x/loggy/LoggyApplication.java
@@ -2,10 +2,22 @@ package x.loggy;
 
 import org.slf4j.LoggerFactory;
 
+import static x.loggy.SeverityConverter.HIGH;
+import static x.loggy.SeverityConverter.LOW;
+import static x.loggy.SeverityConverter.MEDIUM;
+import static x.loggy.SeverityConverter.URGENT;
+
 public class LoggyApplication {
     public static void main(final String... args) {
         final var logger = LoggerFactory.getLogger(LoggyApplication.class);
-        logger.info("Hi, mom!");
+        logger.error("I cannot do that, Dave");
+        logger.warn("Nanites nearby");
+        logger.info("Happy, snappy");
         logger.debug("Should not appear");
+
+        logger.error(URGENT, "Bachman overdrive");
+        logger.error(HIGH, "Turner overturned");
+        logger.error(MEDIUM, "Flip to the B side");
+        logger.error(LOW, "Sodium");
     }
 }
--- custom-logback-pattern/src/main/java/x/loggy/SeverityConverter.java
+++ custom-logback-pattern/src/main/java/x/loggy/SeverityConverter.java
@@ -1,19 +1,38 @@
 package x.loggy;
 
-import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.pattern.ClassicConverter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import org.slf4j.Marker;
+
+import static ch.qos.logback.classic.Level.ERROR;
+import static ch.qos.logback.classic.Level.INFO;
+import static ch.qos.logback.classic.Level.WARN;
+import static org.slf4j.MarkerFactory.getMarker;
 
 public class SeverityConverter
         extends ClassicConverter {
+    private static final String UrgentSeverity = "Urgent";
+    public static final Marker URGENT = getMarker(UrgentSeverity);
+    private static final String HighSeverity = "High";
+    public static final Marker HIGH = getMarker(HighSeverity);
+    private static final String MediumSeverity = "Medium";
+    public static final Marker MEDIUM = getMarker(MediumSeverity);
+    private static final String LowSeverity = "Low";
+    public static final Marker LOW = getMarker(LowSeverity);
+
     @Override
     public String convert(final ILoggingEvent event) {
+        final var marker = event.getMarker();
+        if (null != marker) {
+            return marker.getName();
+        }
+
         final var level = event.getLevel();
-        if (Level.ERROR.equals(level)) return "HIGH";
-        if (Level.WARN.equals(level)) return "MEDIUM";
-        if (Level.INFO.equals(level)) return "LOW";
+        if (ERROR.equals(level)) return HighSeverity;
+        if (WARN.equals(level)) return MediumSeverity;
+        if (INFO.equals(level)) return LowSeverity;
         else throw new IllegalStateException(
-                "Logback appender missing a level filter: unexpected log level: "
-                        + level);
+                "Logback appender missing a level filter: unexpected "
+                        + "log level: " + level);
     }
 }
