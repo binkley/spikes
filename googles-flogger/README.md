1d0c48daebfe5cc00211e16cfeb801a8ebf0c5a1
Brian Oxley <boxley@thoughtworks.com>
2019-05-07 05:46:17 -0500

    Work around unfortunate Gradle

--- googles-flogger/README.md
+++ googles-flogger/README.md
@@ -4,5 +4,5 @@ Try out Google's [Flogger](https://github.com/google/flogger).
 
 ## Run the from command line
 
-1. Run `./gradlew build`.
-2. Run `java -jar build/libs/googles-flogger-0.0.1-SNAPSHOT.jar`.
+1. Run `./gradlew build shadowJar`.
+2. Run `java -jar build/libs/googles-flogger-0.0.1-SNAPSHOT-all.jar`.
--- googles-flogger/src/main/java/x/floggy/FloggyApplication.java
+++ googles-flogger/src/main/java/x/floggy/FloggyApplication.java
@@ -1,6 +1,14 @@
 package x.floggy;
 
+import com.google.common.flogger.FluentLogger;
+
+import static java.util.logging.Level.INFO;
+
 public class FloggyApplication {
     public static void main(final String... args) {
+        final var logger = FluentLogger.forEnclosingClass();
+
+        logger.at(INFO).withCause(new NullPointerException("NULL!"))
+                .log("Well, Bob.");
     }
 }
