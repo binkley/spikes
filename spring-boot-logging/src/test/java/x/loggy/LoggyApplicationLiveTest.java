9feb8a4ca6734fc9813d0332ea4db12a93f83c84
Brian Oxley <boxley@thoughtworks.com>
2019-05-11 07:32:20 -0500

    Sad path test

--- spring-boot-logging/src/test/java/x/loggy/LoggyApplicationLiveTest.java
+++ spring-boot-logging/src/test/java/x/loggy/LoggyApplicationLiveTest.java
@@ -14,6 +14,7 @@ import java.time.Instant;
 
 import static java.time.ZoneOffset.UTC;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
 
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -23,6 +24,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 class LoggyApplicationLiveTest {
     private final Clock clock;
     private final LoggyRemote loggy;
+    private final NotFoundRemote notFound;
 
     @Test
     void shouldSendTracingThroughFeign() {
@@ -34,6 +36,16 @@ class LoggyApplicationLiveTest {
 
         assertThat(response).isEqualTo(
                 new LoggyResponse("HI, MOM!", 22, Instant.now(clock)));
+    }
+
+    @Test
+    void shouldHandleNotFound() {
+        MDC.put("X-B3-TraceId", "abcdef0987654321");
+        MDC.put("X-B3-SpanId", "abcdef0987654321");
+        MDC.put("X-B3-ParentSpanId", "abcdef0987654321");
+
+        assertThatThrownBy(notFound::get)
+                .hasFieldOrPropertyWithValue("status", 404);
     }
 
     @TestConfiguration
