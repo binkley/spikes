53ab9eb38de4548b4705387b01f872637f034995
Brian Oxley <boxley@thoughtworks.com>
2019-05-11 08:26:16 -0500

    Fully local
    You can now run from an airplane.

--- spring-boot-logging/src/main/java/x/loggy/NotFoundRemote.java
+++ spring-boot-logging/src/main/java/x/loggy/NotFoundRemote.java
@@ -3,8 +3,7 @@ package x.loggy;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "not-found",
-        url = "https://jsonplaceholder.typicode.com/not-found")
+@FeignClient(name = "not-found", url = "http://localhost:8080/not-found")
 public interface NotFoundRemote {
     @GetMapping(produces = "application/json")
     String get();
