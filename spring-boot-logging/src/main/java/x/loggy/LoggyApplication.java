67030128ff2317e8babeab9dc44da125d63982c4
Brian Oxley <boxley@thoughtworks.com>
2019-05-11 06:09:50 -0500

    Cleaner code

--- spring-boot-logging/src/main/java/x/loggy/LoggyApplication.java
+++ spring-boot-logging/src/main/java/x/loggy/LoggyApplication.java
@@ -1,118 +1,18 @@
 package x.loggy;
 
-import feign.FeignException;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.core.style.ToStringCreator;
-
-import java.io.IOError;
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
-
-import static org.springframework.core.NestedExceptionUtils.getMostSpecificCause;
 
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @SpringBootApplication
 public class LoggyApplication {
-    private final LoggyRemote loggy;
-    private final SampleHttpBinRemote happyPath;
-    private final NowheresVilleRemote sadPath;
-    private final NotAThingRemote notFound;
-    private final Logger logger;
-
     public static void main(final String... args) {
         // FYI -- using the try-block shuts down the program after
         // the command-line runner finishes: Faster feedback cycle
         try (final var ignored = SpringApplication
                 .run(LoggyApplication.class, args)) {
-        }
-    }
-
-    @EventListener
-    public void ready(final ApplicationReadyEvent event) {
-        logger.warn("SIMPLE LOGGING");
-        logger.info("I am ready: {}", new ToStringCreator(event)
-                .append("args", event.getArgs())
-                .append("source", event.getSource())
-                .append("timestamp", event.getTimestamp()));
-        logger.debug("And this is json: {\"a\":3}"); // Logged as string
-        logger.debug("{\"a\":3}"); // Logged as embedded JSON, not string
-
-        logger.warn("CALL OURSELVES");
-        // Talk to ourselves
-        final var request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("http://localhost:8080"))
-                .headers(
-                        "X-B3-TraceId", "abcdef0987654321",
-                        "X-B3-SpanId", "abcdef0987654321",
-                        "X-B3-ParentSpanId", "abcdef0987654321")
-                .build();
-        final var client = HttpClient.newBuilder()
-                .build();
-
-        final HttpResponse<String> response = sendOrDie(request, client);
-
-        logger.debug("(Really got {} after sending {})", response, request);
-        logger.info("{}", response.body());
-
-        logger.warn("EXCEPTIONS");
-
-        // Show stack trace logging
-        final var e = new NullPointerException("OH MY, A NULL POINTER!");
-        logger.error("And I fail: {}", e.getMessage(), e);
-
-        // Feign
-        logger.warn("FEIGN");
-
-        logger.warn("CALL OURSELVES WITH FEIGN");
-
-        final var loggyResponse = loggy.get();
-        logger.info("We said, {}", loggyResponse);
-
-        final var happyFeign = happyPath.get();
-        logger.info("He said, {}", happyFeign);
-
-        try {
-            sadPath.get();
-        } catch (final FeignException ignored) {
-            // Already logged by logbook-feign logger
-        }
-
-        try {
-            notFound.get();
-        } catch (final FeignException notFound) {
-            logger.error("Feign angry: {}: {}",
-                    getMostSpecificCause(notFound).toString(),
-                    notFound.contentUTF8(),
-                    notFound);
-        }
-
-        logger.warn("BUT IT'S ALRIGHT, IT'S OK, I'M GONNA RUN THAT WAY");
-    }
-
-    private HttpResponse<String> sendOrDie(final HttpRequest request,
-            final HttpClient client) {
-        try {
-            return client.send(request, BodyHandlers.ofString());
-        } catch (final IOException e) {
-            logger.error("SERVER NOT READY? {}",
-                    getMostSpecificCause(e).toString(), e);
-            throw new IOError(e);
-        } catch (final InterruptedException e) {
-            logger.error("INTERRUPTED? {}",
-                    getMostSpecificCause(e).toString(), e);
-            Thread.currentThread().interrupt();
-            return null;
         }
     }
 }
---
+++ spring-boot-logging/src/main/java/x/loggy/LoggyReadyListener.java
@@ -0,0 +1,109 @@
+package x.loggy;
+
+import feign.FeignException;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.style.ToStringCreator;
+import org.springframework.stereotype.Component;
+
+import java.io.IOError;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import static org.springframework.core.NestedExceptionUtils.getMostSpecificCause;
+
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class LoggyReadyListener {
+    private final LoggyRemote loggy;
+    private final SampleHttpBinRemote happyPath;
+    private final NowheresVilleRemote sadPath;
+    private final NotAThingRemote notFound;
+    private final Logger logger;
+
+    @EventListener
+    public void ready(final ApplicationReadyEvent event) {
+        logger.warn("SIMPLE LOGGING");
+        logger.info("I am ready: {}", new ToStringCreator(event)
+                .append("args", event.getArgs())
+                .append("source", event.getSource())
+                .append("timestamp", event.getTimestamp()));
+        logger.debug("And this is json: {\"a\":3}"); // Logged as string
+        logger.debug("{\"a\":3}"); // Logged as embedded JSON, not string
+
+        logger.warn("CALL OURSELVES");
+        // Talk to ourselves
+        final var request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("http://localhost:8080"))
+                .headers(
+                        "X-B3-TraceId", "abcdef0987654321",
+                        "X-B3-SpanId", "abcdef0987654321",
+                        "X-B3-ParentSpanId", "abcdef0987654321")
+                .build();
+        final var client = HttpClient.newBuilder()
+                .build();
+
+        final HttpResponse<String> response = sendOrDie(request, client);
+
+        logger.debug("(Really got {} after sending {})", response, request);
+        logger.info("{}", response.body());
+
+        logger.warn("EXCEPTIONS");
+
+        // Show stack trace logging
+        final var e = new NullPointerException("OH MY, A NULL POINTER!");
+        logger.error("And I fail: {}", e.getMessage(), e);
+
+        // Feign
+        logger.warn("FEIGN");
+
+        logger.warn("CALL OURSELVES WITH FEIGN");
+
+        final var loggyResponse = loggy.get();
+        logger.info("We said, {}", loggyResponse);
+
+        final var happyFeign = happyPath.get();
+        logger.info("He said, {}", happyFeign);
+
+        try {
+            sadPath.get();
+        } catch (final FeignException ignored) {
+            // Already logged by logbook-feign logger
+        }
+
+        try {
+            notFound.get();
+        } catch (final FeignException notFound) {
+            logger.error("Feign angry: {}: {}",
+                    getMostSpecificCause(notFound).toString(),
+                    notFound.contentUTF8(),
+                    notFound);
+        }
+
+        logger.warn("BUT IT'S ALRIGHT, IT'S OK, I'M GONNA RUN THAT WAY");
+    }
+
+    private HttpResponse<String> sendOrDie(final HttpRequest request,
+            final HttpClient client) {
+        try {
+            return client.send(request, BodyHandlers.ofString());
+        } catch (final IOException e) {
+            logger.error("SERVER NOT READY? {}",
+                    getMostSpecificCause(e).toString(), e);
+            throw new IOError(e);
+        } catch (final InterruptedException e) {
+            logger.error("INTERRUPTED? {}",
+                    getMostSpecificCause(e).toString(), e);
+            Thread.currentThread().interrupt();
+            return null;
+        }
+    }
+}
