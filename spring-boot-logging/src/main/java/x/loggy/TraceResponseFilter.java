c1f518cec40baa197b36ad3d753b93af9c6673d2
Brian Oxley <boxley@thoughtworks.com>
2019-05-13 06:08:50 -0400

    Dead code

--- spring-boot-logging/src/main/java/x/loggy/TraceResponseFilter.java
+++ spring-boot-logging/src/main/java/x/loggy/TraceResponseFilter.java
@@ -3,7 +3,6 @@ package x.loggy;
 import brave.Tracer;
 import brave.Tracing;
 import brave.propagation.TraceContext;
-import brave.propagation.TraceContext.Extractor;
 import brave.propagation.TraceContext.Injector;
 import org.slf4j.Logger;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -17,14 +16,12 @@ import java.io.IOException;
 public class TraceResponseFilter
         extends OncePerRequestFilter {
     private final Tracer tracer;
-    private final Extractor<HttpServletRequest> extractor;
     private final Injector<HttpServletResponse> injector;
     private final Logger logger;
 
     public TraceResponseFilter(final Tracing tracing, final Tracer tracer,
             final Logger logger) {
         final var propagation = tracing.propagation();
-        extractor = propagation.extractor(HttpServletRequest::getHeader);
         injector = propagation.injector(
                 HttpServletResponse::setHeader);
         this.tracer = tracer;
