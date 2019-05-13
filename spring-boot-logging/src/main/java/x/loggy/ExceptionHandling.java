244be6da6713068e7d620458c275e994728780ed
Brian Oxley <boxley@thoughtworks.com>
2019-05-13 16:06:22 -0400

    Feign problems are 502

--- spring-boot-logging/src/main/java/x/loggy/ExceptionHandling.java
+++ spring-boot-logging/src/main/java/x/loggy/ExceptionHandling.java
@@ -1,10 +1,16 @@
 package x.loggy;
 
+import feign.FeignException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.zalando.problem.Problem;
 import org.zalando.problem.StatusType;
 import org.zalando.problem.spring.web.advice.ProblemHandling;
 import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
 
+import static org.zalando.problem.Status.BAD_GATEWAY;
 import static org.zalando.problem.Status.UNPROCESSABLE_ENTITY;
 
 @ControllerAdvice
@@ -13,5 +19,11 @@ public class ExceptionHandling
     @Override
     public StatusType defaultConstraintViolationStatus() {
         return UNPROCESSABLE_ENTITY;
+    }
+
+    @ExceptionHandler(FeignException.class)
+    public ResponseEntity<Problem> handleFeign(
+            final FeignException e, final NativeWebRequest request) {
+        return create(BAD_GATEWAY, e, request);
     }
 }
--- spring-boot-logging/src/main/java/x/loggy/ProblemConfiguration.java
+++ spring-boot-logging/src/main/java/x/loggy/ProblemConfiguration.java
@@ -1,11 +1,14 @@
 package x.loggy;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.zalando.problem.ProblemModule;
 import org.zalando.problem.validation.ConstraintViolationProblemModule;
 
 @Configuration
+@EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
 public class ProblemConfiguration {
     @Bean
     public ProblemModule problemModule() {
