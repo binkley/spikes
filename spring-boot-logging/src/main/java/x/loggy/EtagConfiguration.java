04ecadb9f93379a2825b6896307ca4294192e0d3
Brian Oxley <boxley@thoughtworks.com>
2019-05-13 16:22:49 -0400

    Etags in logging - why quoted JSON value?

---
+++ spring-boot-logging/src/main/java/x/loggy/EtagConfiguration.java
@@ -0,0 +1,13 @@
+package x.loggy;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@Configuration
+public class EtagConfiguration {
+    @Bean
+    public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }
+}
