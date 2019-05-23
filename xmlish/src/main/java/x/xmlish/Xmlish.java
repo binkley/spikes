f9a447dc2471fef7b4272af784faa29e7c5ce99b
Brian Oxley <boxley@thoughtworks.com>
2019-05-23 17:57:03 -0500

    Simpler

--- xmlish/src/main/java/x/xmlish/Xmlish.java
+++ xmlish/src/main/java/x/xmlish/Xmlish.java
@@ -1,13 +1,9 @@
 package x.xmlish;
 
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
-import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
 import java.time.Instant;
 
-@JacksonXmlRootElement
-@RequiredArgsConstructor
 @Value
 class Xmlish {
     String foo;
