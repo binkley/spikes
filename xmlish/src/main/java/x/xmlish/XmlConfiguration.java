7e5fd93cfbb7fb37535378c026080d69ddb992b2
Brian Oxley <boxley@thoughtworks.com>
2019-05-23 18:05:28 -0500

    ISO UTC date/time

--- xmlish/src/main/java/x/xmlish/XmlConfiguration.java
+++ xmlish/src/main/java/x/xmlish/XmlConfiguration.java
@@ -5,12 +5,15 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
+
 @Configuration
 public class XmlConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
         final var objectMapper = new XmlMapper();
-        objectMapper.findAndRegisterModules();
+        objectMapper.findAndRegisterModules()
+                .configure(WRITE_DATES_AS_TIMESTAMPS, false);
         return objectMapper;
     }
 }
