651981daa995ef8937cd48ad91cc359deae7ded5
Brian Oxley <boxley@thoughtworks.com>
2019-06-16 17:47:33 -0500

    Demo custom Feign failure logging

--- spring-boot-logging/src/main/java/x/loggy/UnknownHostRemote.java
+++ spring-boot-logging/src/main/java/x/loggy/UnknownHostRemote.java
@@ -3,8 +3,11 @@ package x.loggy;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import static x.loggy.AlertMessage.Severity.HIGH;
+
 @FeignClient(name = "sad-path", url = "https://not.really.a.place/get")
 public interface UnknownHostRemote {
+    @AlertMessage(message = "UNKNOWABLE HOST", severity = HIGH)
     @GetMapping(produces = "application/json")
     String get();
 }
