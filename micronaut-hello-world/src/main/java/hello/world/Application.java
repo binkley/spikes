e8f5a7a20c58babe3e70758eccf8f663083b2240
Brian Oxley <boxley@thoughtworks.com>
2019-06-29 09:18:18 -0500

    Swagger fully working

--- micronaut-hello-world/src/main/java/hello/world/Application.java
+++ micronaut-hello-world/src/main/java/hello/world/Application.java
@@ -3,12 +3,15 @@ package hello.world;
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
 @OpenAPIDefinition(info = @Info(
         title = "Hello World",
         version = "0.1",
-        description = "HI, MOM!"
-))
+        description = "HI, MOM!",
+        license = @License(
+                name = "Public Domain",
+                url = "http://unlicense.org")))
 public class Application {
     public static void main(final String... args) {
         Micronaut.run(Application.class);
---
+++ micronaut-hello-world/src/main/java/hello/world/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package hello.world;
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.core.annotation.Introspected;
+import lombok.Data;
+
+import java.util.List;
+
+@ConfigurationProperties("swagger")
+@Data
+@Introspected
+public class SwaggerConfig {
+    private String version;
+    private String layout;
+    private boolean deepLinking;
+    private List<URIConfig> urls;
+
+    @ConfigurationProperties("urls")
+    @Data
+    public static class URIConfig {
+        private String name;
+        private String url;
+    }
+}
