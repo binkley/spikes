6d120966c4d32e74146f16c6601984116ccc403e
Brian Oxley <boxley@thoughtworks.com>
2019-07-03 07:08:17 -0500

    Introduce a DB

--- kotlin-micronaut/src/main/kotlin/x/micronaut/Application.kt
+++ kotlin-micronaut/src/main/kotlin/x/micronaut/Application.kt
@@ -1,6 +1,9 @@
 package x.micronaut
 
 import io.micronaut.runtime.Micronaut
+import org.jetbrains.exposed.sql.Database
+import javax.annotation.PostConstruct
+import javax.sql.DataSource
 
 object Application {
     @JvmStatic
@@ -9,5 +12,10 @@ object Application {
                 .packages("kotlin.micronaut")
                 .mainClass(Application.javaClass)
                 .start()
+    }
+
+    @PostConstruct
+    fun init(dataSource: DataSource) {
+        Database.connect(dataSource)
     }
 }
