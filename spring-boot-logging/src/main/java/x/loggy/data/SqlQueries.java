452197e97962f8fa93d505b69b2975968045cb96
Brian Oxley <boxley@thoughtworks.com>
2019-08-30 14:01:11 -0500

    Micrometer for SQL calls -- with Prometheus
    Still using the wrong meter, however.

--- spring-boot-logging/src/main/java/x/loggy/data/SqlQueries.java
+++ spring-boot-logging/src/main/java/x/loggy/data/SqlQueries.java
@@ -3,24 +3,56 @@ package x.loggy.data;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import java.util.AbstractList;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 import static ch.qos.logback.classic.Level.DEBUG;
+import static java.util.Locale.US;
 import static java.util.regex.Pattern.compile;
 import static org.slf4j.LoggerFactory.getLogger;
 
-public final class SqlQueries
+@Component
+public class SqlQueries
         extends AbstractList<String> {
     private static final Pattern queryOnly = compile(
             "^Executing prepared SQL statement \\[(.*)]$");
     private final List<String> queries = new ArrayList<>();
     @SuppressWarnings("unused")
     private final Appender appender = new Appender();
+
+    private final Map<String, DistributionSummary> histograms
+            = new HashMap<>();
+
+    public SqlQueries(final MeterRegistry registry) {
+        for (final var type : List
+                .of("SELECT", "INSERT", "UPDATE", "OTHER", "INVALID"))
+            histograms.put(type, DistributionSummary.builder("database.calls")
+                    .tags("sql", type.toLowerCase())
+                    .publishPercentiles(0.50, 0.90, 0.95, 0.99)
+                    .register(registry));
+    }
+
+    private static String bucket(final String query) {
+        try {
+            final var statement = CCJSqlParserUtil.parse(query);
+            return statement.getClass().getSimpleName()
+                    .replace("Statement", "")
+                    .toUpperCase(US);
+        } catch (final JSQLParserException e) {
+            return "INVALID";
+        }
+    }
 
     @Override
     public int size() {
@@ -54,7 +86,21 @@ public final class SqlQueries
             final var message = eventObject.getMessage();
             final var matcher = queryOnly.matcher(message);
             if (!matcher.find()) return;
-            queries.add(matcher.group(1));
+            final var query = matcher.group(1);
+            queries.add(query);
+
+            final var bucket = bucket(query);
+            switch (bucket) {
+            case "SELECT":
+            case "INSERT":
+            case "UPDATE":
+            case "INVALID":
+                histograms.get(bucket).record(1);
+                break;
+            default:
+                histograms.get("OTHER").record(1);
+                break;
+            }
         }
     }
 }
--- spring-boot-logging/src/test/java/x/loggy/data/BobDataTest.java
+++ spring-boot-logging/src/test/java/x/loggy/data/BobDataTest.java
@@ -2,22 +2,44 @@ package x.loggy.data;
 
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import static java.lang.String.format;
+import static java.net.http.HttpClient.newHttpClient;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @AutoConfigureEmbeddedDatabase
-@DataJdbcTest
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@SpringBootTest(properties = {
+        "logging.level.x.loggy=WARN",
+        "loggy.enable-demo=false"
+}, webEnvironment = RANDOM_PORT)
 class BobDataTest {
     private final BobRepository repository;
+    private final SqlQueries queries;
+
+    @LocalServerPort
+    private int port;
+
+    @AfterEach
+    void tearDown() {
+        queries.clear();
+    }
 
     @Test
-    void shouldRoundTrip() {
-        final var queries = new SqlQueries();
-
+    void shouldRoundTrip()
+            throws IOException, InterruptedException {
         final var unsaved = new BobRecord();
         unsaved.name = "William";
         final var saved = repository.save(unsaved);
@@ -28,6 +50,14 @@ class BobDataTest {
         assertThat(queries).hasSize(2);
         assertThat(queries.get(0)).startsWith("INSERT");
         assertThat(queries.get(1)).startsWith("SELECT");
-        queries.clear();
+
+        final var client = newHttpClient();
+        final var response = client.send(HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(format(
+                        "http://localhost:%d/actuator/prometheus", port)))
+                .build(), BodyHandlers.ofString(UTF_8));
+
+        assertThat(response.body()).contains("database_calls{sql=\"select\"");
     }
 }
