b2bc4a690b833d4688b90f44814a1ce902541400
Brian Oxley <boxley@thoughtworks.com>
2019-09-07 07:53:44 -0500

    Buckets for repositories

--- spring-boot-logging/src/main/java/x/loggy/data/BobRepository.java
+++ spring-boot-logging/src/main/java/x/loggy/data/BobRepository.java
@@ -1,9 +1,55 @@
 package x.loggy.data;
 
+import io.micrometer.core.annotation.Timed;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface BobRepository
         extends CrudRepository<BobRecord, Long> {
+    @Override
+    @Timed("bob.repository")
+    <S extends BobRecord> S save(S entity);
+
+    @Override
+    @Timed("bob.repository")
+    <S extends BobRecord> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @Timed("bob.repository")
+    Optional<BobRecord> findById(Long aLong);
+
+    @Override
+    @Timed("bob.repository")
+    boolean existsById(Long aLong);
+
+    @Override
+    @Timed("bob.repository")
+    Iterable<BobRecord> findAll();
+
+    @Override
+    @Timed("bob.repository")
+    Iterable<BobRecord> findAllById(Iterable<Long> longs);
+
+    @Override
+    @Timed("bob.repository")
+    long count();
+
+    @Override
+    @Timed("bob.repository")
+    void deleteById(Long aLong);
+
+    @Override
+    @Timed("bob.repository")
+    void delete(BobRecord entity);
+
+    @Override
+    @Timed("bob.repository")
+    void deleteAll(Iterable<? extends BobRecord> entities);
+
+    @Override
+    @Timed("bob.repository")
+    void deleteAll();
 }
