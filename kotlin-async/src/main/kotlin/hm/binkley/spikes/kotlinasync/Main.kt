82ab66659e5aa1d328e6e159d7040a19667c05e0
Brian Oxley <boxley@thoughtworks.com>
2019-09-25 18:02:28 -0500

    Approximate async properties (but not quite)

--- kotlin-async/src/main/kotlin/hm/binkley/spikes/kotlinasync/Main.kt
+++ kotlin-async/src/main/kotlin/hm/binkley/spikes/kotlinasync/Main.kt
@@ -8,6 +8,24 @@ fun main() {
     }
 }
 
+class Bar {
+    val foo = Foo(3)
+}
+
 suspend fun foo() {
     println("Foo!")
+    val bar = Bar()
+    println(bar.foo())
+    bar.foo(2)
+    println(bar.foo())
 }
+
+class Foo(private var foo: Int) {
+    suspend fun getFoo() = foo
+    suspend fun setFoo(foo: Int) {
+        this.foo = foo
+    }
+}
+
+suspend operator fun Foo.invoke() = getFoo()
+suspend operator fun Foo.invoke(foo: Int) = setFoo(foo)
