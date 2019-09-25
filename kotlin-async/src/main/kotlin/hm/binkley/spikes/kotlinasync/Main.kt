8d4d5418d732869000cb33647014f2c7728fb4df
Brian Oxley <boxley@thoughtworks.com>
2019-09-25 17:48:34 -0500

    First async function

--- kotlin-async/src/main/kotlin/hm/binkley/spikes/kotlinasync/Main.kt
+++ kotlin-async/src/main/kotlin/hm/binkley/spikes/kotlinasync/Main.kt
@@ -1,4 +1,13 @@
 package hm.binkley.spikes.kotlinasync
 
+import kotlinx.coroutines.runBlocking
+
 fun main() {
+    runBlocking {
+        foo()
+    }
+}
+
+suspend fun foo() {
+    println("Foo!")
 }
