cc53c1e8dc19d334c84686de243ddc9c16ea0f40
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-09-27 06:41:54 -0500

    Generated from Spring Initialzr

---
+++ spring-boot-import-cloud-versions/src/main/java/x/springbootimportcloudversions/SpringBootImportCloudVersionsApplication.java
@@ -0,0 +1,13 @@
+package x.springbootimportcloudversions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootImportCloudVersionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootImportCloudVersionsApplication.class, args);
+	}
+
+}
