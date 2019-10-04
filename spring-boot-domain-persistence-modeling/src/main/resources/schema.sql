118f499837a6f4212fd5ec30e950eac890d980e9
Brian Oxley <boxley@thoughtworks.com>
2019-10-04 13:10:57 -0500

    Schema updates

--- spring-boot-domain-persistence-modeling/src/main/resources/schema.sql
+++ spring-boot-domain-persistence-modeling/src/main/resources/schema.sql
@@ -12,14 +12,14 @@ CREATE TABLE child
 (
     id         SERIAL PRIMARY KEY,
     natural_id VARCHAR NOT NULL UNIQUE,
-    parent_id  INT REFERENCES parent (id),
+    parent_id  INT REFERENCES parent (id), -- Nullable
     value      VARCHAR,
     version    INT,
     created_at TIMESTAMP,
     updated_at TIMESTAMP
 );
 
-CREATE OR REPLACE FUNCTION immutable_natural_key_f()
+CREATE OR REPLACE FUNCTION update_immutable_natural_key_f()
     RETURNS TRIGGER
     LANGUAGE plpgsql
 AS
@@ -64,14 +64,17 @@ BEGIN
         RETURN NULL;
     END IF;
 
-    -- Ignore if caller tried to replace the version, so "old.version + 1"
+    IF (new.version <> old.version) THEN
+        RAISE 'Outdated: %, %', old.version, new.version;
+    END IF;
+
     new.version := old.version + 1;
     new.updated_at = now();
     RETURN new;
 END;
 $$;
 
-CREATE OR REPLACE FUNCTION insert_child_parent_f()
+CREATE OR REPLACE FUNCTION insert_child_update_parent_f()
     RETURNS TRIGGER
     LANGUAGE plpgsql
 AS
@@ -87,7 +90,7 @@ BEGIN
 END;
 $$;
 
-CREATE OR REPLACE FUNCTION update_child_parent_f()
+CREATE OR REPLACE FUNCTION update_child_update_parent_f()
     RETURNS TRIGGER
     LANGUAGE plpgsql
 AS
@@ -103,7 +106,7 @@ BEGIN
 END;
 $$;
 
-CREATE OR REPLACE FUNCTION delete_child_parent_f()
+CREATE OR REPLACE FUNCTION delete_child_update_parent_f()
     RETURNS TRIGGER
     LANGUAGE plpgsql
 AS
@@ -119,56 +122,56 @@ BEGIN
 END;
 $$;
 
-CREATE TRIGGER immutable_parent_natural_key_t
+CREATE TRIGGER a_update_parent_immutable_natural_key_t
     BEFORE UPDATE
     ON parent
     FOR EACH ROW
-EXECUTE PROCEDURE immutable_natural_key_f();
+EXECUTE PROCEDURE update_immutable_natural_key_f();
 
-CREATE TRIGGER insert_parent_audit_t
+CREATE TRIGGER b_insert_parent_audit_t
     BEFORE INSERT
     ON parent
     FOR EACH ROW
 EXECUTE PROCEDURE insert_audit_f();
 
-CREATE TRIGGER update_parent_audit_t
+CREATE TRIGGER b_update_parent_audit_t
     BEFORE UPDATE
     ON parent
     FOR EACH ROW
 EXECUTE PROCEDURE update_audit_f();
 
-CREATE TRIGGER immutable_child_natural_key_t
+CREATE TRIGGER a_update_child_immutable_natural_key_t
     BEFORE UPDATE
     ON child
     FOR EACH ROW
-EXECUTE PROCEDURE immutable_natural_key_f();
+EXECUTE PROCEDURE update_immutable_natural_key_f();
 
-CREATE TRIGGER insert_child_audit_t
+CREATE TRIGGER b_insert_child_audit_t
     BEFORE INSERT
     ON child
     FOR EACH ROW
 EXECUTE PROCEDURE insert_audit_f();
 
-CREATE TRIGGER update_child_audit_t
+CREATE TRIGGER b_update_child_audit_t
     BEFORE UPDATE
     ON child
     FOR EACH ROW
 EXECUTE PROCEDURE update_audit_f();
 
-CREATE TRIGGER insert_child_parent_t
+CREATE TRIGGER c_insert_child_update_parent_t
     AFTER INSERT
     ON child
     FOR EACH ROW
-EXECUTE PROCEDURE insert_child_parent_f();
+EXECUTE PROCEDURE insert_child_update_parent_f();
 
-CREATE TRIGGER update_child_parent_t
+CREATE TRIGGER c_update_child_update_parent_t
     AFTER UPDATE
     ON child
     FOR EACH ROW
-EXECUTE PROCEDURE update_child_parent_f();
+EXECUTE PROCEDURE update_child_update_parent_f();
 
-CREATE TRIGGER delete_child_parent_t
+CREATE TRIGGER c_delete_child_update_parent_t
     AFTER DELETE
     ON child
     FOR EACH ROW
-EXECUTE PROCEDURE delete_child_parent_f();
+EXECUTE PROCEDURE delete_child_update_parent_f();
