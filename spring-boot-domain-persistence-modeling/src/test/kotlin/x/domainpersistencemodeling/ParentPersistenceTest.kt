c5e461f679048fbc6c7f46ab191fbcf2d26e8f8d
Brian Oxley <boxley@thoughtworks.com>
2019-10-05 11:26:38 -0500

    Finally, tests pass

--- spring-boot-domain-persistence-modeling/src/test/kotlin/x/domainpersistencemodeling/ParentPersistenceTest.kt
+++ spring-boot-domain-persistence-modeling/src/test/kotlin/x/domainpersistencemodeling/ParentPersistenceTest.kt
@@ -2,7 +2,10 @@ package x.domainpersistencemodeling
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE
 
+@AutoConfigureTestDatabase(replace = NONE)
 @DataJdbcTest
 class ParentPersistenceTest {
     @Test
