275088bea9c64a1d0d7b3a966ffd4cf8d36b92ab
Brian Oxley <boxley@thoughtworks.com>
2019-10-10 05:25:35 -0500

    WIP

--- micronaut-domain-persistence-modeling/src/test/kotlin/x/domainpersistencemodeling/TestListener.kt
+++ micronaut-domain-persistence-modeling/src/test/kotlin/x/domainpersistencemodeling/TestListener.kt
@@ -2,23 +2,24 @@ package x.domainpersistencemodeling
 
 import ch.tutteli.atrium.creating.ReportingAssertionPlant
 import ch.tutteli.atrium.verbs.expect
-import io.micronaut.context.event.ApplicationEvent
 import io.micronaut.context.event.ApplicationEventListener
 import javax.inject.Singleton
 
 @Singleton
-class TestListener<E : ApplicationEvent> : ApplicationEventListener<E> {
-    private val received = mutableListOf<E>()
+class TestListener<Event : DomainChangedEvent<*>>
+    : ApplicationEventListener<Event> {
+    private val received = mutableListOf<Event>()
 
     fun reset() {
         received.clear()
     }
 
-    override fun onApplicationEvent(event: E) {
+    override fun onApplicationEvent(event: Event) {
         received.add(event)
     }
 
-    val expectNext: ReportingAssertionPlant<List<E>>
+    /** Creates a `List` expectation for AssertJ, and resets the listener. */
+    val expectNext: ReportingAssertionPlant<List<Event>>
         get() = expect(received.toList()).also {
             reset()
         }
