1a6a3d18a38bd10de3c5c5a14eafc5c900f5f69b
Brian Oxley <boxley@thoughtworks.com>
2019-10-12 18:13:35 -0500

    Mutable subchildren; needs tests

---
+++ spring-boot-domain-persistence-modeling-java/src/main/java/x/scratch/TrackedSortedSet.java
@@ -0,0 +1,62 @@
+package x.scratch;
+
+import javax.annotation.Nonnull;
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.function.BiConsumer;
+
+public class TrackedSortedSet<T extends Comparable<T>> extends AbstractSet<T> {
+    private final BiConsumer<T, Set<T>> added;
+    private final BiConsumer<T, Set<T>> removed;
+    private final Set<T> sorted;
+
+    public TrackedSortedSet(final Set<T> initial,
+            final BiConsumer<T, Set<T>> added,
+            final BiConsumer<T, Set<T>> removed) {
+        this.added = added;
+        this.removed = removed;
+        sorted = new TreeSet<>(initial);
+    }
+
+    @Nonnull
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private final Iterator<T> it = sorted.iterator();
+            private T curr = null;
+
+            @Override
+            public boolean hasNext() {
+                return it.hasNext();
+            }
+
+            @Override
+            public T next() {
+                curr = it.next();
+                return curr;
+            }
+
+            @Override
+            public void remove() {
+                it.remove();
+                removed.accept(curr, sorted);
+            }
+        };
+    }
+
+    @Override
+    public int size() {
+        return sorted.size();
+    }
+
+    @Override
+    public boolean add(final T t) {
+        final var add = sorted.add(t);
+        if (add) {
+            added.accept(t, sorted);
+        }
+        return add;
+    }
+}
--- spring-boot-domain-persistence-modeling-java/src/main/java/x/scratch/child/PersistedMutableChild.java
+++ spring-boot-domain-persistence-modeling-java/src/main/java/x/scratch/child/PersistedMutableChild.java
@@ -4,12 +4,58 @@ import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
-import lombok.experimental.Delegate;
+import x.scratch.TrackedSortedSet;
+
+import javax.annotation.Nonnull;
+import java.util.Set;
 
 @EqualsAndHashCode
 @RequiredArgsConstructor
 @ToString
 public final class PersistedMutableChild implements MutableChild {
-    @Delegate(types = MutableChild.class)
     private final @NonNull ChildRecord record;
+
+    @Nonnull
+    @Override
+    public String getNaturalId() {
+        return record.getNaturalId();
+    }
+
+    @Override
+    public String getParentNaturalId() {
+        return record.getParentNaturalId();
+    }
+
+    @Override
+    public void setParentNaturalId(final String parentNaturalId) {
+        record.setParentNaturalId(parentNaturalId);
+    }
+
+    @Override
+    public String getValue() {
+        return record.getValue();
+    }
+
+    @Override
+    public void setValue(final String newValue) {
+        record.setValue(newValue);
+    }
+
+    @Nonnull
+    @Override
+    public Set<String> getSubchildren() {
+        return new TrackedSortedSet<>(record.getSubchildren(),
+                this::resetSubchildrenToPreserveSorting, this::resetSubchildrenToPreserveSorting);
+    }
+
+    @Override
+    public int getVersion() {
+        return record.getVersion();
+    }
+
+    private void resetSubchildrenToPreserveSorting(
+            final String item, final Set<String> all) {
+        record.getSubchildren().clear();
+        record.getSubchildren().addAll(all);
+    }
 }
