cbeabfa8e2fc98435d021a3d6d6c04eaeb7609ea
Brian Oxley <boxley@thoughtworks.com>
2019-10-20 14:24:28 -0500

    Keep following Getting Started - test

--- quarkus-hello/src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ quarkus-hello/src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -4,18 +4,30 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static java.util.UUID.randomUUID;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class GreetingResourceTest {
-
+class GreetingResourceTest {
     @Test
-    public void testHelloEndpoint() {
+    void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("hello"));
     }
 
+    @Test
+    void testGreetingEndpoint() {
+        // TODO: Ick, flakey test
+        final String aName = randomUUID().toString();
+
+        given()
+                .pathParam("name", aName)
+                .when().get("/hello/greeting/{name}", aName)
+                .then()
+                .statusCode(200)
+                .body(is("Hello, " + aName + "!"));
+    }
 }
