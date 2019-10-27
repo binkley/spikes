cfceaf1bffacfbfd72efad67ed7e2e37312abc65
Brian Oxley <boxley@thoughtworks.com>
2019-10-27 08:55:15 -0500

    Better manual testing cycle

--- layers-kt/run.sh
+++ layers-kt/run.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 cd ~/tmp
 
 rm -rf layers.git layers
 git init --bare layers.git
 git clone layers.git
+
+cd layers
+
+git commit --allow-empty -m Init
+git push -u origin master
