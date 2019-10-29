2d39e17b6ec8e0250d115e1f6bd760554897d2e8
Brian Oxley <boxley@thoughtworks.com>
2019-10-29 17:28:16 -0500

    Superfically, this works with Spring Boot 2.2

--- spring-boot-async-and-mockito-spies/src/test/kotlin/x/scratch/ScratchApplicationTests.kt
+++ spring-boot-async-and-mockito-spies/src/test/kotlin/x/scratch/ScratchApplicationTests.kt
@@ -1,14 +1,22 @@
 package x.scratch
 
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.SpyBean
 
 @SpringBootTest
-class ScratchApplicationTests @Autowired constructor(
-        private val sally: Sally) {
+class ScratchApplicationTests {
+    @SpyBean
+    private lateinit var bob: Bob
+    @Autowired
+    private lateinit var sally: Sally
+
     @Test
     fun shouldWaitOnBob() {
         sally.runIt()
+
+        verify(bob).runItEventually()
     }
 }
