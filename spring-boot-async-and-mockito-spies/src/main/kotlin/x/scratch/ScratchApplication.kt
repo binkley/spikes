96ea797d4a47d6051ee1a1690afd5ccc7afbc2e0
Brian Oxley <boxley@thoughtworks.com>
2019-10-29 16:56:04 -0500

    Init from Spring Initalizr

---
+++ spring-boot-async-and-mockito-spies/src/main/kotlin/x/scratch/ScratchApplication.kt
@@ -0,0 +1,11 @@
+package x.scratch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScratchApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScratchApplication>(*args)
+}
---
+++ spring-boot-async-and-mockito-spies/src/test/kotlin/x/scratch/ScratchApplicationTests.kt
@@ -0,0 +1,13 @@
+package x.scratch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScratchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
