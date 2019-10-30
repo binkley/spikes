35ef1d36584a10acd0ef3ded7ecc29222d3d13e0
Brian Oxley <boxley@thoughtworks.com>
2019-10-30 05:58:27 -0500

    Set the stage

--- spring-boot-async-and-mockito-spies-java/src/main/java/x/scratch/ScratchApplication.java
+++ spring-boot-async-and-mockito-spies-java/src/main/java/x/scratch/ScratchApplication.java
@@ -1,11 +1,33 @@
 package x.scratch;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
 
+@EnableAsync
 @SpringBootApplication
 public class ScratchApplication {
     public static void main(final String[] args) {
         SpringApplication.run(ScratchApplication.class, args);
+    }
+
+    @Component
+    @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+    public static class Sally {
+        private final Bob bob;
+
+        public void runIt() {
+            bob.runItEventually();
+        }
+    }
+
+    @Component
+    public static class Bob {
+        @Async
+        public void runItEventually() {}
     }
 }
