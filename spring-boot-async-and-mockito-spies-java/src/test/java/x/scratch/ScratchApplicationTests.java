1abc79194f96811aac4d150959ce08c1bc9d2ef0
Brian Oxley <boxley@thoughtworks.com>
2019-10-30 05:47:50 -0500

    Init

---
+++ spring-boot-async-and-mockito-spies-java/src/test/java/x/scratch/ScratchApplicationTests.java
@@ -0,0 +1,16 @@
+package x.scratch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScratchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
