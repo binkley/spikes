42fab9f6257635e32c7d56e5da9024794f5f0463
Brian Oxley <boxley@thoughtworks.com>
2019-10-30 05:51:17 -0500

    Formatting

--- spring-boot-async-and-mockito-spies-java/src/main/java/x/scratch/ScratchApplication.java
+++ spring-boot-async-and-mockito-spies-java/src/main/java/x/scratch/ScratchApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScratchApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ScratchApplication.class, args);
-	}
-
+    public static void main(final String[] args) {
+        SpringApplication.run(ScratchApplication.class, args);
+    }
 }
