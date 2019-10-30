ff2f16f8d45e771ea528a06fb82c6fc395530090
Brian Oxley <boxley@thoughtworks.com>
2019-10-30 17:05:41 -0500

    Smarter testing

--- spring-boot-async-and-mockito-spies/src/test/kotlin/x/scratch/ScratchApplicationTests.kt
+++ spring-boot-async-and-mockito-spies/src/test/kotlin/x/scratch/ScratchApplicationTests.kt
@@ -7,53 +7,30 @@ import org.mockito.Mockito.timeout
 import org.mockito.Mockito.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.test.mock.mockito.SpyBean
-import org.springframework.context.annotation.Bean
-import org.springframework.core.task.TaskExecutor
+import org.springframework.context.annotation.Import
 import java.util.concurrent.TimeUnit.SECONDS
-import java.util.concurrent.atomic.AtomicBoolean
 
+@Import(MyTestConfiguration::class)
 @SpringBootTest
 class ScratchApplicationTests {
-    companion object {
-        private val executorRan = AtomicBoolean(false)
-        private val taskRan = AtomicBoolean(false)
-    }
-
     @SpyBean
     private lateinit var bob: Bob
     @Autowired
     private lateinit var sally: Sally
+    @Autowired
+    private lateinit var testingExecutor: TestingExecutor
 
     @Test
     fun shouldWaitOnBob() {
-        assertThat(mockingDetails(bob).isSpy).`as`("Should be a spy")
+        assertThat(mockingDetails(bob).isSpy)
+                .`as`("Should be a spy")
                 .isTrue()
 
         sally.runIt()
 
-        try {
-            verify(bob, timeout(SECONDS.toMillis(2L))).runItEventually()
-        } catch (e: AssertionError) {
-            if (!executorRan.get())
-                throw AssertionError("Did not even run the executor")
-            if (!taskRan.get())
-                throw AssertionError("Did not even run the task")
-            throw e
-        }
-    }
+        testingExecutor.awaitTaskRan(1L, SECONDS)
 
-    @TestConfiguration
-    class MyTestConfiguration {
-        @Bean
-        fun slowExecutor() = TaskExecutor { task ->
-            executorRan.set(true)
-            Thread {
-                SECONDS.sleep(1L)
-                taskRan.set(true)
-                task.run()
-            }.start()
-        }
+        verify(bob, timeout(SECONDS.toMillis(2L))).runItEventually()
     }
 }
