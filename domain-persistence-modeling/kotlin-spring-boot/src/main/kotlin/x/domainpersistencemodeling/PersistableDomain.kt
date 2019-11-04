845a403e8c03a689c8f5d5a0508e6fb78a2fe833
Brian Oxley <boxley@thoughtworks.com>
2019-11-04 06:14:25 -0600

    Grammar

--- domain-persistence-modeling/kotlin-spring-boot/src/main/kotlin/x/domainpersistencemodeling/PersistableDomain.kt
+++ domain-persistence-modeling/kotlin-spring-boot/src/main/kotlin/x/domainpersistencemodeling/PersistableDomain.kt
@@ -8,7 +8,7 @@ interface PersistableDomain<Resource,
         get() = 0 < version
 
     /**
-     * Optimizes to lessen the number of persistence calls.  The code should
+     * Optimizes to lessen the count of persistence calls.  The code should
      * be correct with or without this optimization: for example, a domain
      * object version number should have the same values.
      */
