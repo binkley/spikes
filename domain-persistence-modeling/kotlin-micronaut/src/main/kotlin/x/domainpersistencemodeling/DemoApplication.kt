8409ce5ca97af7d6e1aa360c47198fd520c65905
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-11-25 08:14:28 -0600

    Run production in UTC

--- domain-persistence-modeling/kotlin-micronaut/src/main/kotlin/x/domainpersistencemodeling/DemoApplication.kt
+++ domain-persistence-modeling/kotlin-micronaut/src/main/kotlin/x/domainpersistencemodeling/DemoApplication.kt
@@ -1,10 +1,14 @@
 package x.domainpersistencemodeling
 
 import io.micronaut.runtime.Micronaut
+import java.time.ZoneOffset.UTC
+import java.util.TimeZone
 
 object DemoApplication {
     @JvmStatic
     fun main(args: Array<String>) {
+        TimeZone.setDefault(TimeZone.getTimeZone(UTC))
+
         Micronaut.build()
             .packages("x")
             .mainClass(DemoApplication.javaClass)
