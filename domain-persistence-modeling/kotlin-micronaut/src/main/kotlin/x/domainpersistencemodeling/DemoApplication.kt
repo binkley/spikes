e56e3476d9d36c996c63b220739bb4e6c31d9375
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-12-03 09:12:16 -0600

    Bolthole for coverage

--- domain-persistence-modeling/kotlin-micronaut/src/main/kotlin/x/domainpersistencemodeling/DemoApplication.kt
+++ domain-persistence-modeling/kotlin-micronaut/src/main/kotlin/x/domainpersistencemodeling/DemoApplication.kt
@@ -1,9 +1,11 @@
 package x.domainpersistencemodeling
 
 import io.micronaut.runtime.Micronaut
+import lombok.Generated
 import java.time.ZoneOffset.UTC
 import java.util.TimeZone
 
+@Generated
 object DemoApplication {
     @JvmStatic
     fun main(args: Array<String>) {
