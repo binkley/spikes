feeb98606d53a97a1fdafdec4c3308e3a5488bab
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-12-07 07:04:41 -0600

    Try typeOf

--- scratch/src/main/kotlin/x/scratch/scratch.kt
+++ scratch/src/main/kotlin/x/scratch/scratch.kt
@@ -1,2 +1,20 @@
 package x.scratch
 
+import lombok.Generated
+import kotlin.reflect.typeOf
+
+@Generated // Lie to JaCoCo
+@UseExperimental(ExperimentalStdlibApi::class)
+fun main() {
+    val intType = typeOf<Int>()
+    println(intType)
+
+    accessReifiedTypeArg<String>()
+    accessReifiedTypeArg<List<String>>()
+}
+
+@UseExperimental(ExperimentalStdlibApi::class)
+inline fun <reified T> accessReifiedTypeArg() {
+    val kType = typeOf<T>()
+    println(kType.toString())
+}
