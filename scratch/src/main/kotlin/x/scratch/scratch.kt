ee18d755fb296c21d5679ba1c32b231f10900f5c
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-12-09 06:09:43 -0600

    Example unsigned types

--- scratch/src/main/kotlin/x/scratch/scratch.kt
+++ scratch/src/main/kotlin/x/scratch/scratch.kt
@@ -1,8 +1,14 @@
 package x.scratch
 
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind.EXACTLY_ONCE
+import kotlin.contracts.contract
 import kotlin.reflect.typeOf
 
-@UseExperimental(ExperimentalStdlibApi::class)
+@UseExperimental(
+    ExperimentalStdlibApi::class,
+    ExperimentalUnsignedTypes::class
+)
 fun main() {
     val intType = typeOf<Int>()
     println(intType)
@@ -10,9 +16,16 @@ fun main() {
     accessReifiedTypeArg<String>()
     accessReifiedTypeArg<List<String>>()
 
-    val first = MyClass(1)
-    val second = MyClass(1)
+    val first = InlineClass(1)
+    val second = InlineClass(1)
     println(first == second)
+    println(first::class)
+
+    foo(second)
+
+    val x: UByte = 255u
+
+    println("$x -> ${x.toByte()}")
 }
 
 @UseExperimental(ExperimentalStdlibApi::class)
@@ -21,4 +34,23 @@ inline fun <reified T> accessReifiedTypeArg() {
     println(kType.toString())
 }
 
-inline class MyClass(val value: Int)
+inline class InlineClass(val value: Int)
+
+@UseExperimental(ExperimentalContracts::class)
+fun synchronize(lock: Any?, block: () -> Unit) {
+    // It tells compiler:
+    // "This function will invoke 'block' here and now, and exactly one time"
+    contract { callsInPlace(block, EXACTLY_ONCE) }
+
+    block()
+}
+
+fun foo(lock: Any) {
+    val x: Int
+    synchronize(lock) {
+        x = 42 // Compiler knows that lambda passed to 'synchronize' is called
+        // exactly once, so no reassignment is reported
+    }
+    println(x) // Compiler knows that lambda will be definitely called, performing
+    // initialization, so 'x' is considered to be initialized here
+}
