c10cd7d198d17ffac9396d2f9703a991a5320b0d
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-12-12 13:01:13 -0600

    Less bulky

--- layers-kt/src/main/kotlin/hm/binkley/layers/Layers.kt
+++ layers-kt/src/main/kotlin/hm/binkley/layers/Layers.kt
@@ -1,7 +1,5 @@
 package hm.binkley.layers
 
-import java.util.Objects
-
 interface Diffable {
     fun toDiff(): String
 }
@@ -60,83 +58,4 @@ interface Layers : AutoCloseable {
             : Layer
 
     fun newLayer(): Layer
-}
-
-typealias Rule<T> = (RuleContext<T>) -> T
-
-open class Value<T>(val rule: Rule<T>?, val value: T?) : Diffable {
-    protected fun printSimple(value: Any?) = when (value) {
-        is String -> "\"${value}\""
-        else -> value.toString()
-    }
-
-    // TODO: PersistedValue
-    open fun toSourceCode() = printSimple(value)
-
-    // TODO: Print "3 : Int" rather than just "3"
-    //  Expression in a class literal has a nullable type 'T', use !! to make
-    //  the type non-nullable
-    override fun toDiff() = if (null == rule) "$value" else "$this"
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Value<*>
-
-        return rule == other.rule
-                && value == other.value
-    }
-
-    override fun hashCode() =
-        Objects.hash(rule, value)
-
-    override fun toString() =
-        "${this::class.simpleName}{rule=$rule, value=$value}"
-}
-
-open class RuleValue<T>(val name: String, val default: T, rule: Rule<T>) :
-    Value<T>(rule, default) {
-    override fun toSourceCode(): String {
-        return rule.toString()
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as RuleValue<*>
-
-        return name == other.name
-                && default == other.default
-                && rule == other.rule
-    }
-
-    override fun hashCode() =
-        Objects.hash(name, default, rule)
-
-    override fun toString() =
-        "${this::class.simpleName}<rule: $name[=$default]>"
-}
-
-fun <T> value(context: T): Value<T> =
-    Value(null, context)
-
-fun <T> rule(name: String, default: T, rule: Rule<T>) =
-    RuleValue(name, default, rule)
-
-class RuleContext<T>(
-    val myKey: String,
-    private val layers: LayersForRuleContext
-) {
-    val myValues: List<T>
-        get() = layers.allValuesFor(myKey)
-
-    operator fun <T> get(key: String) = layers.appliedValueFor<T>(key)
-}
-
-interface LayersForRuleContext {
-    fun <T> appliedValueFor(key: String): T
-
-    fun <T> allValuesFor(key: String): List<T>
 }
