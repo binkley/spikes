06a0653d5a7062049e1189403241c58c267c2831
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-12-12 08:53:03 -0600

    Cleaner

--- micronaut-retryable/src/test/kotlin/x/retryable/RetryingTest.kt
+++ micronaut-retryable/src/test/kotlin/x/retryable/RetryingTest.kt
@@ -1,15 +1,11 @@
 package x.retryable
 
-import ch.qos.logback.classic.spi.ILoggingEvent
-import ch.qos.logback.core.read.ListAppender
 import ch.tutteli.atrium.api.cc.en_GB.hasSize
 import ch.tutteli.atrium.api.cc.en_GB.toThrow
 import ch.tutteli.atrium.verbs.expect
 import io.micronaut.http.client.exceptions.HttpClientException
-import io.micronaut.retry.intercept.DefaultRetryInterceptor
 import io.micronaut.test.annotation.MicronautTest
 import org.junit.jupiter.api.Test
-import org.slf4j.LoggerFactory.getLogger
 import javax.inject.Inject
 
 @MicronautTest
@@ -19,19 +15,13 @@ internal class RetryingTest {
 
     @Test
     fun `should retry 3 times`() { // No, not really :)
-        val retryingLogger = getLogger(DefaultRetryInterceptor::class.java)
-                as ch.qos.logback.classic.Logger
-        // TODO: Consider a more specific appender for the task
-        //  See: https://github.com/binkley/spikes/blob/master/domain-persistence-modeling/kotlin-micronaut/src/test/kotlin/x/domainpersistencemodeling/SqlQueries.kt
-        val listAppender = ListAppender<ILoggingEvent>()
-        listAppender.start()
-        retryingLogger.addAppender(listAppender)
+        val testRetryAppender = TestRetryAppender()
 
         expect {
             retrying.retryMe()
         }.toThrow<HttpClientException> { }
 
-        expect(listAppender.list.filter {
+        expect(testRetryAppender.events.filter {
             it.message.startsWith("Retrying")
         }).hasSize(RetryingClient.attempts.toInt())
     }
---
+++ micronaut-retryable/src/test/kotlin/x/retryable/TestRetryAppender.kt
@@ -0,0 +1,39 @@
+package x.retryable
+
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.AppenderBase
+import io.micronaut.retry.intercept.DefaultRetryInterceptor
+import org.slf4j.LoggerFactory.getLogger
+
+internal class TestRetryAppender : AppenderBase<ILoggingEvent>() {
+    private val logger = getLogger(DefaultRetryInterceptor::class.java)
+            as Logger
+    private val oldLevel = logger.level
+    private val oldAdditive = logger.isAdditive
+
+    private val _events = mutableListOf<ILoggingEvent>()
+    val events: List<ILoggingEvent>
+        get() = _events
+
+    fun reset() = _events.clear()
+
+    override fun append(eventObject: ILoggingEvent) {
+        _events += eventObject
+    }
+
+    override fun stop() {
+        super.stop()
+        reset()
+        logger.level = oldLevel
+        logger.isAdditive = oldAdditive
+    }
+
+    init {
+        logger.level = Level.DEBUG
+        logger.isAdditive = false // Avoid extra DEBUG console logging
+        logger.addAppender(this)
+        start()
+    }
+}
