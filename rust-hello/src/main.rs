58307edc60921b21de967cc0e8d620ecc22a0c0d
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-12-14 06:02:15 -0600

    Work through debug and display

--- rust-hello/src/main.rs
+++ rust-hello/src/main.rs
@@ -1,6 +1,19 @@
+use std::fmt;
 use std::io::{stdout, BufWriter};
 
 use ferris_says::say;
+
+#[derive(Debug)]
+struct Structure(i32);
+
+impl fmt::Display for Structure {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Debug)]
+struct Deep(Structure);
 
 fn main() {
     let stdout = stdout();
@@ -9,4 +22,8 @@ fn main() {
 
     let mut writer = BufWriter::new(stdout.lock());
     say(out, width, &mut writer).unwrap();
+
+    println!("Now {} will print!", Structure(2));
+    println!("Now {:?} will print!", Structure(3));
+    println!("Now {:?} will print!", Deep(Structure(7)));
 }
