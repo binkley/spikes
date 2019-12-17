21de7ed40c3a2cf6019064ab8fcbde261e483b99
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-12-17 16:54:09 -0600

    Spike out map box

--- rust-hello/src/lib.rs
+++ rust-hello/src/lib.rs
@@ -28,6 +28,125 @@ mod tests {
     }
 }
 
+pub mod boxy {
+    use std::borrow::Borrow;
+    use std::collections::hash_map::{Iter, IterMut};
+    use std::collections::HashMap;
+    use std::hash::Hash;
+    use std::iter::FromIterator;
+    use std::ops::Index;
+
+    #[derive(Clone, Debug, Eq)]
+    pub struct MapBox<K: Eq + Hash, V> {
+        map: HashMap<K, V>,
+    }
+
+    impl<K: Hash + Eq, V> MapBox<K, V> {
+        pub fn new() -> MapBox<K, V> {
+            MapBox {
+                map: HashMap::new(),
+            }
+        }
+
+        #[inline]
+        pub fn iter(&self) -> Iter<'_, K, V> {
+            self.map.iter()
+        }
+
+        #[inline]
+        pub fn iter_mut(&mut self) -> IterMut<'_, K, V> {
+            self.map.iter_mut()
+        }
+    }
+
+    impl<K, V> PartialEq for MapBox<K, V>
+    where
+        K: Eq + Hash,
+        V: PartialEq,
+    {
+        fn eq(&self, other: &MapBox<K, V>) -> bool {
+            self.map == other.map
+        }
+    }
+
+    impl<K, V> Default for MapBox<K, V>
+    where
+        K: Eq + Hash,
+    {
+        #[inline]
+        fn default() -> Self {
+            MapBox::new()
+        }
+    }
+
+    impl<K, Q: ?Sized, V> Index<&Q> for MapBox<K, V>
+    where
+        K: Eq + Hash + Borrow<Q>,
+        Q: Eq + Hash,
+    {
+        type Output = V;
+
+        #[inline]
+        fn index(&self, key: &Q) -> &V {
+            self.map.index(key)
+        }
+    }
+
+    impl<'a, K: Hash + Eq, V> IntoIterator for &'a MapBox<K, V> {
+        type Item = (&'a K, &'a V);
+        type IntoIter = Iter<'a, K, V>;
+
+        #[inline]
+        fn into_iter(self) -> Self::IntoIter {
+            self.iter()
+        }
+    }
+
+    impl<'a, K: Hash + Eq, V> IntoIterator for &'a mut MapBox<K, V> {
+        type Item = (&'a K, &'a mut V);
+        type IntoIter = IterMut<'a, K, V>;
+
+        #[inline]
+        fn into_iter(self) -> Self::IntoIter {
+            self.iter_mut()
+        }
+    }
+
+    impl<K, V> FromIterator<(K, V)> for MapBox<K, V>
+    where
+        K: Eq + Hash,
+    {
+        fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> MapBox<K, V> {
+            let mut map = MapBox::new();
+            map.extend(iter);
+            map
+        }
+    }
+
+    impl<K, V> Extend<(K, V)> for MapBox<K, V>
+    where
+        K: Eq + Hash,
+    {
+        #[inline]
+        fn extend<T: IntoIterator<Item = (K, V)>>(&mut self, iter: T) {
+            self.map.extend(iter)
+        }
+    }
+
+    /* TODO
+    impl<'a, K, V> Extend<(&'a K, &'a V)> for MapBox<K, V>
+    where
+        K: Eq + Hash + Copy,
+        V: Copy,
+    {
+        #[inline]
+        fn extend<T: IntoIterator<Item = (&'a K, &'a V)>>(&mut self, iter: T) {
+            self.map.extend(iter)
+        }
+    }
+    */
+}
+
 pub mod math {
     // TODO: generic `Unsigned`, u128
     pub fn collatz(x: u64) -> u32 {
