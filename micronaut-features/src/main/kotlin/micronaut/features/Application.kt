3f2e79029744f8c8804613618d78b87b768f326c
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-12-22 08:48:13 -0600

    Init from mn

---
+++ micronaut-features/src/main/kotlin/micronaut/features/Application.kt
@@ -0,0 +1,22 @@
+package micronaut.features
+
+import io.micronaut.runtime.Micronaut
+import io.swagger.v3.oas.annotations.*
+import io.swagger.v3.oas.annotations.info.*
+
+@OpenAPIDefinition(
+    info = Info(
+            title = "micronaut-features",
+            version = "0.0"
+    )
+)
+object Application {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.build()
+                .packages("micronaut.features")
+                .mainClass(Application.javaClass)
+                .start()
+    }
+}
