06b518d896d4060598d3525b655255de1edff81f
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-12-27 07:40:43 -0600

    Friendlier

--- kotlin-blockchain/src/main/kotlin/x/scratch/blockchain/blockchain.kt
+++ kotlin-blockchain/src/main/kotlin/x/scratch/blockchain/blockchain.kt
@@ -6,12 +6,20 @@ import java.util.Objects
 import kotlin.Int.Companion.MAX_VALUE
 
 fun main() {
-    val block = Block.first("00")
-    println("$block")
-    println("${block.next("Hello, world!")}")
+    val firstBlock = Block.first("00")
+    println("$firstBlock")
+    val nextBlock = firstBlock.next("Hello, world!")
+    println("$nextBlock")
 }
 
 private val sha256 = MessageDigest.getInstance("SHA-256")
+
+fun Block.Companion.first(difficulty: String) =
+    Block(
+        "Genesis",
+        "0".repeat(64),
+        difficulty
+    )
 
 class Block(
     val data: String,
@@ -24,10 +32,9 @@ class Block(
     fun next(data: String) = Block(data, hash, difficulty)
 
     private fun hashWithProofOfWork(): String {
-        fun hashWithNonce(nonce: Int) =
-            sha256
-                .digest((nonce.toString() + timestamp.toString() + previousHash + data).toByteArray())
-                .joinToString("") { "%02x".format(it) }
+        fun hashWithNonce(nonce: Int) = sha256
+            .digest("$nonce$timestamp$previousHash$data".toByteArray())
+            .joinToString("") { "%02x".format(it) }
 
         for (nonce in 0..MAX_VALUE) {
             val hash = hashWithNonce(nonce)
@@ -46,13 +53,7 @@ class Block(
     override fun hashCode() = Objects.hash(hash)
 
     override fun toString() =
-        "${super.toString()}{data=$data, previousHash=$previousHash, timestamp=$timestamp, hash=$hash}"
+        "${super.toString()}{timestamp=$timestamp, data=$data, hash=$hash, previousHash=$previousHash}"
 
-    companion object {
-        fun first(difficulty: String) = Block(
-            "Genesis",
-            "0000000000000000000000000000000000000000000000000000000000000000",
-            difficulty
-        )
-    }
+    companion object
 }
