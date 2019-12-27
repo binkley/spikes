9db137a137bcc634c8ff989ad4c612d0b67d22af
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-12-27 07:48:44 -0600

    Real or fake time: you pick

--- kotlin-blockchain/src/main/kotlin/x/scratch/blockchain/blockchain.kt
+++ kotlin-blockchain/src/main/kotlin/x/scratch/blockchain/blockchain.kt
@@ -2,34 +2,46 @@ package x.scratch.blockchain
 
 import java.security.MessageDigest
 import java.time.Instant
+import java.time.Instant.EPOCH
 import java.util.Objects
 import kotlin.Int.Companion.MAX_VALUE
 
 fun main() {
-    val firstBlock = Block.first("00")
+    var firstBlock = Block.first("00")
     println("$firstBlock")
-    val nextBlock = firstBlock.next("Hello, world!")
+    var nextBlock = firstBlock.next("Hello, world!")
+    println("$nextBlock")
+
+    // Testing example
+    firstBlock = Block.first(timestamp = EPOCH)
+    println("$firstBlock")
+    nextBlock = firstBlock.next("Hello, world!", Instant.ofEpochMilli(1L))
     println("$nextBlock")
 }
 
 private val sha256 = MessageDigest.getInstance("SHA-256")
 
-fun Block.Companion.first(difficulty: String = "") =
+fun Block.Companion.first(
+    difficulty: String = "",
+    timestamp: Instant = Instant.now()
+) =
     Block(
         "Genesis",
         "0".repeat(64),
-        difficulty
+        difficulty,
+        timestamp
     )
 
 class Block(
     val data: String,
     val previousHash: String,
     val difficulty: String,
-    val timestamp: Instant = Instant.now()
+    val timestamp: Instant
 ) {
     val hash: String = hashWithProofOfWork()
 
-    fun next(data: String) = Block(data, hash, difficulty)
+    fun next(data: String, timestamp: Instant = Instant.now()) =
+        Block(data, hash, difficulty, timestamp)
 
     private fun hashWithProofOfWork(): String {
         fun hashWithNonce(nonce: Int) = sha256
