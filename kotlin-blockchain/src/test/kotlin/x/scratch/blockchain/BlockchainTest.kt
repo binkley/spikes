47c018af0040812469a3c4f51dff50b4d6638ec6
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-12-29 10:46:32 -0600

    It runs tests

---
+++ kotlin-blockchain/src/test/kotlin/x/scratch/blockchain/BlockchainTest.kt
@@ -0,0 +1,10 @@
+package x.scratch.blockchain
+
+import org.junit.jupiter.api.Test
+
+internal class BlockchainTest {
+    @Test
+    fun should() {
+        val blockchain = Blockchain.new()
+    }
+}
