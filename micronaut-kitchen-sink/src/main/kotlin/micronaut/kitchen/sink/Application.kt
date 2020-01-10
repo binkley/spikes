9070480652243ea1cc9f80f80b378c8af2fc5047
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-01-10 10:59:34 -0600

    New spike
    Created with:
    
    ```
    mn create-app micronaut-kitchen-sink -l kotlin -b maven -f http-client,http-server,junit,logback,management,micrometer-prometheus,swagger-kotlin,tracing-jaeger
    ```

---
+++ micronaut-kitchen-sink/src/main/kotlin/micronaut/kitchen/sink/Application.kt
@@ -0,0 +1,22 @@
+package micronaut.kitchen.sink
+
+import io.micronaut.runtime.Micronaut
+import io.swagger.v3.oas.annotations.*
+import io.swagger.v3.oas.annotations.info.*
+
+@OpenAPIDefinition(
+    info = Info(
+            title = "micronaut-kitchen-sink",
+            version = "0.0"
+    )
+)
+object Application {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Micronaut.build()
+                .packages("micronaut.kitchen.sink")
+                .mainClass(Application.javaClass)
+                .start()
+    }
+}
