9a29acf301432ac077249907cf2b3bfd23abd0d4
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-01-12 10:33:35 -0600

    More Swagger

--- micronaut-kitchen-sink/src/main/kotlin/hm/binkley/scratch/Application.kt
+++ micronaut-kitchen-sink/src/main/kotlin/hm/binkley/scratch/Application.kt
@@ -2,7 +2,9 @@ package hm.binkley.scratch
 
 import io.micronaut.runtime.Micronaut
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
 import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
 import lombok.Generated
 import java.time.ZoneOffset.UTC
 import java.util.TimeZone
@@ -10,8 +12,18 @@ import java.util.TimeZone
 @Generated
 @OpenAPIDefinition( // TODO: Annotate package, not class
     info = Info(
-        title = "micronaut-kitchen-sink",
-        version = "0.0"
+        contact = Contact(
+            name = "B. K. Oxley (binkley)",
+            email = "binkley@alumni.rice.edu",
+            url = "https://github.com/binkley"
+        ),
+        description = "Experiments in Micronaut features",
+        license = License(
+            name = "Public Domain",
+            url = "https://unlicense.org/"
+        ),
+        title = "Micronaut Kitchen Sink",
+        version = "0.1"
     )
 )
 object Application {
