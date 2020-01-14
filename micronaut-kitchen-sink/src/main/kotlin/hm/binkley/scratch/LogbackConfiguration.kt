d2063e8ab4437450f1e198480466dacff5576eb2
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-01-14 07:43:34 -0600

    Provide default Micronaut console logging format
    Disabled for now until I figure out how to inject the environment, and toggle
    json vs text logging.

--- micronaut-kitchen-sink/src/main/kotlin/hm/binkley/scratch/LogbackConfiguration.kt
+++ micronaut-kitchen-sink/src/main/kotlin/hm/binkley/scratch/LogbackConfiguration.kt
@@ -5,19 +5,32 @@ import ch.qos.logback.classic.Level.INFO
 import ch.qos.logback.classic.Logger
 import ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME
 import ch.qos.logback.classic.LoggerContext
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.Appender
 import ch.qos.logback.core.ConsoleAppender
 import net.logstash.logback.encoder.LogstashEncoder
 
-class LogbackConfiguration : BasicConfigurator() {
+class LogbackConfiguration(
+    private val json: Boolean = true
+) : BasicConfigurator() {
     override fun configure(lc: LoggerContext) {
         val console = ConsoleAppender<ILoggingEvent>().apply {
             context = lc
             name = "STDOUT"
-            encoder = LogstashEncoder().apply {
-                context = lc
-            }.also { it.start() }
+            if (json) {
+                encoder = LogstashEncoder().apply {
+                    context = lc
+                }.also { it.start() }
+            } else {
+                isWithJansi = true
+                encoder = PatternLayoutEncoder().apply {
+                    context = lc
+                    pattern = """
+%cyan(%d{HH:mm:ss.SSS}) %gray([%thread]) %highlight(%-5level) %magenta(%logger{36}) - %msg%n
+                    """.trim()
+                }.also { it.start() }
+            }
         }.also { it.start() }
 
         lc.getLogger(ROOT_LOGGER_NAME).apply {
