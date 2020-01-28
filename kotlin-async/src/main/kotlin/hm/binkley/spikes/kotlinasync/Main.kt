8eef33e971932a69cb624640268f19e259a774c6
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-01-28 05:43:15 -0600

    Nicer producer-consumer

--- kotlin-async/src/main/kotlin/hm/binkley/spikes/kotlinasync/Main.kt
+++ kotlin-async/src/main/kotlin/hm/binkley/spikes/kotlinasync/Main.kt
@@ -1,9 +1,15 @@
 package hm.binkley.spikes.kotlinasync
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
+@ExperimentalCoroutinesApi
 fun main() {
     runBlocking {
         foo()
@@ -15,6 +21,8 @@ fun main() {
         launch {
             quux(channel)
         }
+
+        squares().consumeEach { println(it) }
     }
 }
 
@@ -34,6 +42,11 @@ suspend fun qux(channel: Channel<Int>) {
     // this might be heavy CPU-consuming computation or async logic, we'll just send five squares
     for (x in 1..5) channel.send(x * x)
     channel.close()
+}
+
+@ExperimentalCoroutinesApi
+fun CoroutineScope.squares(): ReceiveChannel<Int> = produce(capacity = 5) {
+    for (x in 1..5) send(x * x)
 }
 
 suspend fun quux(channel: Channel<Int>) {
