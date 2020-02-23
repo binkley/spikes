bae5270106d48a87c6f07e31e812ae29d1b3c16c
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-02-23 10:33:10 -0600

    More pretty printing

--- scratch/src/main/kotlin/x/scratch/layers/values.kt
+++ scratch/src/main/kotlin/x/scratch/layers/values.kt
@@ -12,6 +12,8 @@ data class RuleValue<T>(val rule: Rule<T>, val initialValue: T) :
 }
 
 data class BooleanValue(override val value: Boolean) : Value<Boolean>() {
+    override fun toString() = value.toString()
+
     companion object {
         fun lastRule(initialValue: Boolean) =
             RuleValue(LastRule(), initialValue)
@@ -19,6 +21,8 @@ data class BooleanValue(override val value: Boolean) : Value<Boolean>() {
 }
 
 data class IntValue(override val value: Int) : Value<Int>() {
+    override fun toString() = value.toString()
+
     companion object {
         fun sumRule(initialValue: Int) =
             RuleValue(SumRule(), initialValue)
@@ -26,6 +30,8 @@ data class IntValue(override val value: Int) : Value<Int>() {
 }
 
 data class StringValue(override val value: String) : Value<String>() {
+    override fun toString() = value
+
     companion object {
         fun lastRule(initialValue: String) =
             RuleValue(LastRule(), initialValue)
