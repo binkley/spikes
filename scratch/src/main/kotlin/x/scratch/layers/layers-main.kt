b803d66cdca7058f729e7f4957044c389c82f36e
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-02-23 10:43:03 -0600

    Better demo

--- scratch/src/main/kotlin/x/scratch/layers/layers-main.kt
+++ scratch/src/main/kotlin/x/scratch/layers/layers-main.kt
@@ -6,6 +6,8 @@ import x.scratch.layers.Layers.Companion.newLayer
 import x.scratch.layers.StringValue.Companion.lastRule
 
 fun main() {
+    println("INITIAL")
+
     val layers = newLayer(
         "BOB",
         "edit me",
@@ -14,32 +16,41 @@ fun main() {
         "mind" to sumRule(10),
         "invisible" to lastRule(false)
     )
-
-    println(layers)
-    println(layers["name"])
+    dump(layers)
 
     println("---")
+    println("EDIT WITHOUT KEEPING")
 
     layers.edit {
         this["name"] = "Sleepy Bob"
     }
-
-    println(layers)
-    println(layers["name"])
+    dump(layers)
 
     println("---")
+    println("RESETTING")
 
     layers.reset()
-    println(layers)
-    println(layers["name"])
+    dump(layers)
 
     println("---")
+    println("EDIT AND KEEP")
 
-    layers.top.reset("also edit me")
+    layers.reset("also edit me")
     layers.edit {
         this["name"] = "The Cool-hand Bob"
+        this["invisible"] = true
     }
-    layers.keepAndNext("More to do")
+    layers.keepAndNext("more to do")
+    dump(layers)
+}
+
+fun dump(layers: Layers) {
     println(layers)
-    println(layers["name"])
+    println(layers.top)
+    println(
+        """
+        My name: ${layers["name"]}
+        I am invisible: ${layers["invisible"]}
+    """.trimIndent()
+    )
 }
