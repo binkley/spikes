adfcb799658499b6ecdcbcbf8c18eda4e34dff43
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-02-24 11:34:07 -0600

    Clumsy casting for pipe

--- scratch/src/main/kotlin/x/scratch/pipes.kt
+++ scratch/src/main/kotlin/x/scratch/pipes.kt
@@ -1,11 +1,13 @@
 package x.scratch
 
+typealias PIPE<T, U> = (T) -> U
+
 fun main() {
     val double = { it: Int -> 2 * it }
 
     listOf(1, 2, 3) / double / { println(it) }
     // TODO: Does not compile: "{ 2 * it }" inferred to return Unit
-    // listOf(1, 2, 3) / { 2 * it } / { println(it) }
+    listOf(1, 2, 3) / { it: Int -> 2 * it } as PIPE / { println(it) }
 }
 
 operator fun <T, U> Iterable<T>.div(pipe: (T) -> U): Iterable<U> = map(pipe)
