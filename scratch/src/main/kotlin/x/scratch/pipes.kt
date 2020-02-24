43e62ca4dd8f1c076706ad4e3fc5d5a622b1f8f3
Brian Oxley <boxley@thoughtworks.com>
2020-02-24 16:57:13 -0600

    Simpler and cleaner

--- scratch/src/main/kotlin/x/scratch/pipes.kt
+++ scratch/src/main/kotlin/x/scratch/pipes.kt
@@ -1,14 +1,7 @@
 package x.scratch
 
-typealias PIPE<T, U> = (T) -> U
-
 fun main() {
-    val double = { it: Int -> 2 * it }
-
-    listOf(1, 2, 3) / double / { println(it) }
-    // TODO: Does not compile: "{ 2 * it }" inferred to return Unit
-    listOf(1, 2, 3) / { it: Int -> 2 * it } as PIPE / { println(it) }
+    listOf(1, 2, 3) / { 2 * it } / { println(it) }
 }
 
-operator fun <T, U> Iterable<T>.div(pipe: (T) -> U): Iterable<U> = map(pipe)
-operator fun <T> Iterable<T>.div(pipe: (T) -> Unit): Unit = forEach(pipe)
+operator fun <T, U> Iterable<T>.div(pipe: (T) -> U) = map(pipe)
