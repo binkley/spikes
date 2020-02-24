56229ac13f3d07a8fb85c39cc505e4d764092ff9
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-02-24 09:49:16 -0600

    Example Kotlin pipeline
    Inspired by C++ Ranges.

---
+++ scratch/src/main/kotlin/x/scratch/pipes.kt
@@ -0,0 +1,12 @@
+package x.scratch
+
+fun main() {
+    val double = { it: Int -> 2 * it }
+
+    listOf(1, 2, 3) / double / { println(it) }
+    // TODO: Does not compile: "{ 2 * it}" inferred to return Unit
+    // listOf(1, 2, 3) / { 2 * it } / { println(it) }
+}
+
+operator fun <T, U> Iterable<T>.div(pipe: (T) -> U): Iterable<U> = map(pipe)
+operator fun <T> Iterable<T>.div(pipe: (T) -> Unit): Unit = forEach(pipe)
