d7572f88dec233f7176e8447f5a3d75a9bda8941
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-05-15 09:08:16 -0500

    Init boot23 demo

---
+++ boot23/src/main/kotlin/hm/binkley/boot23/Boot23Application.kt
@@ -0,0 +1,11 @@
+package hm.binkley.boot23
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Boot23Application
+
+fun main(args: Array<String>) {
+	runApplication<Boot23Application>(*args)
+}
---
+++ boot23/src/test/kotlin/hm/binkley/boot23/Boot23ApplicationTests.kt
@@ -0,0 +1,13 @@
+package hm.binkley.boot23
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Boot23ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
