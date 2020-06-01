f6cadb49f7634289a4e7ac7b7df1f7f4c1f4d8c5
Brian Oxley <boxley@thoughtworks.com>
2020-06-01 08:21:44 -0500

    Run any scratch file

--- scratch/run.sh
+++ scratch/run.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 jar=target/scratch-0-SNAPSHOT-jar-with-dependencies.jar
 
 test -r $jar || ./mvnw -C package
 
-exec java -jar $jar
+case $# in
+0) class="-jar $jar" ;;
+1) class="-cp $jar x.scratch.${1^}Kt" ;;
+*)
+    echo "Usage: $0 [FILE-NAME]" >&2
+    exit 2
+    ;;
+esac
+
+exec java $class
