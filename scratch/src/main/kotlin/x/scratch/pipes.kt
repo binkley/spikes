52dad49792bea839d6889f54a0b09327778669cc
Brian Oxley <boxley@thoughtworks.com>
2020-06-03 08:49:36 -0500

    Pipes

--- scratch/src/main/kotlin/x/scratch/pipes.kt
+++ scratch/src/main/kotlin/x/scratch/pipes.kt
@@ -1,7 +1,10 @@
 package x.scratch
 
 fun main() {
-    listOf(1, 2, 3) / { 2 * it } / { println(it) }
+    println("== PIPES")
+    val input = listOf(1, 2, 3)
+    println("DOUBLING $input ->")
+    input / { 2 * it } / { println(it) }
 }
 
 operator fun <T, U> Iterable<T>.div(pipe: (T) -> U) = map(pipe)
