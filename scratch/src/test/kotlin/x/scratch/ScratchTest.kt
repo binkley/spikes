296786869bc42751d39a6e3a2eda6c75d1282e46
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-06-12 06:55:41 -0500

    Example coroutine test

--- scratch/src/test/kotlin/x/scratch/ScratchTest.kt
+++ scratch/src/test/kotlin/x/scratch/ScratchTest.kt
@@ -1,9 +1,20 @@
 package x.scratch
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
+@ExperimentalCoroutinesApi
 internal class ScratchTest {
     @Test
-    fun nada() {
+    fun `should test suspending functions`() = runBlockingTest {
+        assertThrows<IllegalStateException> { failing() }
     }
+}
+
+private suspend fun failing() {
+    delay(1L) // pretend
+    error("Fail on purpose")
 }
