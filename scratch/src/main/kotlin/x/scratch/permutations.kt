bbd8d340597751b5d512e884040288345a34d73f
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-06-15 06:52:58 -0500

    Levi-Cevita

--- scratch/src/main/kotlin/x/scratch/permutations.kt
+++ scratch/src/main/kotlin/x/scratch/permutations.kt
@@ -3,19 +3,44 @@ package x.scratch
 fun main() {
     println("== PERMUTATIONS")
     println(
-        "${arrayOf(1, 2, 3).contentToString()} -> ${arrayOf(
+        "SIGNATURE ${arrayOf(1, 2, 3).contentToString()} -> ${arrayOf(
             1, 2, 3
         ).signature}"
     )
     println(
-        "${arrayOf(2, 1, 3).contentToString()} -> ${arrayOf(
+        "SIGNATURE ${arrayOf(2, 1, 3).contentToString()} -> ${arrayOf(
             2, 1, 3
         ).signature}"
     )
     println(
-        "${arrayOf(2, 3, 1).contentToString()} -> ${arrayOf(
+        "SIGNATURE ${arrayOf(2, 3, 1).contentToString()} -> ${arrayOf(
             2, 3, 1
         ).signature}"
+    )
+    println(
+        "SIGNATURE ${arrayOf(1, 1, 1).contentToString()} -> ${arrayOf(
+            1, 1, 1
+        ).signature}"
+    )
+    println(
+        "LEVI-CEVITA ${arrayOf(1, 2, 3).contentToString()} -> ${arrayOf(
+            1, 2, 3
+        ).leviCevita}"
+    )
+    println(
+        "LEVI-CEVITA ${arrayOf(2, 1, 3).contentToString()} -> ${arrayOf(
+            2, 1, 3
+        ).leviCevita}"
+    )
+    println(
+        "LEVI-CEVITA ${arrayOf(2, 3, 1).contentToString()} -> ${arrayOf(
+            2, 3, 1
+        ).leviCevita}"
+    )
+    println(
+        "LEVI-CEVITA ${arrayOf(1, 1, 1).contentToString()} -> ${arrayOf(
+            1, 1, 1
+        ).leviCevita}"
     )
 }
 
@@ -24,8 +49,21 @@ val Array<Int>.signature: Int
         var inversions = 0
         for (i in 0 until size) {
             val n = this[i]
-            for (j in i until size)
+            for (j in i + 1 until size)
                 if (n > this[j]) ++inversions
+        }
+        return if (0 == inversions % 2) 1 else -1
+    }
+
+val Array<Int>.leviCevita: Int
+    get() {
+        var inversions = 0
+        for (i in 0 until size) {
+            val n = this[i]
+            for (j in i + 1 until size) when {
+                n == this[j] -> return 0
+                n > this[j] -> ++inversions
+            }
         }
         return if (0 == inversions % 2) 1 else -1
     }
