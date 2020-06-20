8636240f4c3cca40194674b8b5cee1f4a3e51603
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-06-20 09:34:43 -0500

    More tests

--- scratch/src/test/kotlin/x/scratch/FloatingPointTest.kt
+++ scratch/src/test/kotlin/x/scratch/FloatingPointTest.kt
@@ -3,6 +3,7 @@ package x.scratch
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import x.scratch.BigRational.Companion.NEGATIVE_INFINITY
@@ -12,197 +13,225 @@ import x.scratch.BigRational.Companion.POSITIVE_INFINITY
 import x.scratch.BigRational.Companion.ZERO
 
 internal class FloatingPointTest {
-    @Test
-    fun `should round trip from and to big decimal`() {
-        for (n in listOf(
-            BDouble.TEN,
-            BDouble.ONE,
-            BDouble.ZERO,
-            BDouble.valueOf(1, 1), // 0.1
-            BDouble.valueOf(1, 2), // 0.01
-            BDouble.valueOf(1, 1) + BDouble.valueOf(2, 1), // 0.1+0.2
-            BDouble.valueOf(2,) / BDouble.valueOf(3), // 2.0/3.0
-            -BDouble.ONE,
-            -BDouble.valueOf(1, 1) // -0.1
-        )) assertEquals(
-            n, n.toBigRational().toBigDecimal(),
-            "EQ? -> big decimal <-> rational"
-        )
-    }
-
-    @Test
-    fun `should round trip from and to double`() {
-        for (n in listOf(
-            10.0,
-            1.0,
-            0.0,
-            0.1,
-            0.01,
-            0.1 + 0.2,
-            2.0 / 3.0,
-            -1.0,
-            -0.1,
-            // -0.0, -- TODO: What to do about "negative 0"?
-            Double.MAX_VALUE,
-            -Double.MAX_VALUE,
-            Double.MIN_VALUE,
-            -Double.MIN_VALUE,
-            Double.POSITIVE_INFINITY,
-            Double.NEGATIVE_INFINITY,
-            Double.NaN
-        )) assertTrue(
-            n.eq(n.toBigRational().toDouble()),
-            "EQ? double <-> rational -> $n"
-        )
-    }
-
-    @Test
-    fun `should round trip from and to float`() {
-        for (n in listOf(
-            10.0f,
-            1.0f,
-            0.0f,
-            0.1f,
-            0.01f,
-            0.1f + 0.2f,
-            2.0f / 3.0f,
-            -1.0f,
-            -0.1f,
-            // -0.0, -- TODO: What to do about "negative 0"?
-            Float.MAX_VALUE,
-            -Float.MAX_VALUE,
-            Float.MIN_VALUE,
-            -Float.MIN_VALUE,
-            Float.POSITIVE_INFINITY,
-            Float.NEGATIVE_INFINITY,
-            Float.NaN
-        )) assertTrue(
-            n.eq(n.toBigRational().toFloat()),
-            "EQ? float <-> rational -> $n"
-        )
-    }
-
-    @Test
-    fun `should round trip from and to big integer`() {
-        for (n in listOf(
-            BInt.TEN,
-            BInt.ONE,
-            BInt.ZERO,
-            -BInt.ONE
-        )) assertEquals(
-            n,
-            n.toBigRational().toBigInteger(),
-            "EQ? big integer <-> rational -> $n"
-        )
-    }
-
-    @Test
-    fun `should round trip from and to long`() {
-        for (n in listOf(
-            10L,
-            1L,
-            0L,
-            -1L
-        )) assertEquals(
-            n,
-            n.toBigRational().toLong(),
-            "EQ? long <-> rational -> $n"
-        )
-    }
-
-    @Test
-    fun `should round trip from and to int`() {
-        for (n in listOf(
-            10,
-            1,
-            0,
-            -1
-        )) assertEquals(
-            n,
-            n.toBigRational().toInt(),
-            "EQ? int <-> rational -> $n"
-        )
-    }
-
-    @Test
-    fun `should round trip from and to short`() {
-        assertThrows<UnsupportedOperationException> {
-            ONE.toShort()
-        }
-    }
-
-    @Test
-    fun `should round trip from and to char`() {
-        assertThrows<UnsupportedOperationException> {
-            ONE.toChar()
-        }
-    }
-
-    @Test
-    fun `should round trip from and to byte`() {
-        assertThrows<UnsupportedOperationException> {
-            ONE.toByte()
-        }
-    }
-
-    @Test
-    fun `should compare as primitives do`() {
-        listOf(
-            0.0 to ZERO,
-            +1.0 to +ONE,
-            -1.0 to -ONE,
-            Double.POSITIVE_INFINITY to POSITIVE_INFINITY,
-            Double.NEGATIVE_INFINITY to NEGATIVE_INFINITY,
-            Double.NaN to NaN
-        ).cartesian().forEach { (a, b) ->
-            assertEquals(
-                a.primitive == b.primitive,
-                a.rational == b.rational,
-                "EQ? -> ${a.rational} == ${b.rational}"
-            )
-            assertEquals(
-                a.primitive > b.primitive,
-                a.rational > b.rational,
-                "GT? -> ${a.rational} > ${b.rational}"
-            )
-            assertEquals(
-                a.primitive < b.primitive,
-                a.rational < b.rational,
-                "LT? -> ${a.rational} < ${b.rational}"
+    @Nested
+    inner class Conversions {
+        @Test
+        fun `should round trip from and to big decimal`() {
+            for (n in listOf(
+                BDouble.TEN,
+                BDouble.ONE,
+                BDouble.ZERO,
+                BDouble.valueOf(1, 1), // 0.1
+                BDouble.valueOf(1, 2), // 0.01
+                BDouble.valueOf(1, 1) + BDouble.valueOf(2, 1), // 0.1+0.2
+                BDouble.valueOf(2,) / BDouble.valueOf(3), // 2.0/3.0
+                -BDouble.ONE,
+                -BDouble.valueOf(1, 1) // -0.1
+            )) assertEquals(
+                n, n.toBigRational().toBigDecimal(),
+                "EQ? -> big decimal <-> rational"
             )
         }
+
+        @Test
+        fun `should round trip from and to double`() {
+            for (n in listOf(
+                10.0,
+                1.0,
+                0.0,
+                0.1,
+                0.01,
+                0.1 + 0.2,
+                2.0 / 3.0,
+                -1.0,
+                -0.1,
+                // -0.0, -- TODO: What to do about "negative 0"?
+                Double.MAX_VALUE,
+                -Double.MAX_VALUE,
+                Double.MIN_VALUE,
+                -Double.MIN_VALUE,
+                Double.POSITIVE_INFINITY,
+                Double.NEGATIVE_INFINITY,
+                Double.NaN
+            )) assertTrue(
+                n.eq(n.toBigRational().toDouble()),
+                "EQ? double <-> rational -> $n"
+            )
+        }
+
+        @Test
+        fun `should round trip from and to float`() {
+            for (n in listOf(
+                10.0f,
+                1.0f,
+                0.0f,
+                0.1f,
+                0.01f,
+                0.1f + 0.2f,
+                2.0f / 3.0f,
+                -1.0f,
+                -0.1f,
+                // -0.0, -- TODO: What to do about "negative 0"?
+                Float.MAX_VALUE,
+                -Float.MAX_VALUE,
+                Float.MIN_VALUE,
+                -Float.MIN_VALUE,
+                Float.POSITIVE_INFINITY,
+                Float.NEGATIVE_INFINITY,
+                Float.NaN
+            )) assertTrue(
+                n.eq(n.toBigRational().toFloat()),
+                "EQ? float <-> rational -> $n"
+            )
+        }
+
+        @Test
+        fun `should round trip from and to big integer`() {
+            for (n in listOf(
+                BInt.TEN,
+                BInt.ONE,
+                BInt.ZERO,
+                -BInt.ONE
+            )) assertEquals(
+                n,
+                n.toBigRational().toBigInteger(),
+                "EQ? big integer <-> rational -> $n"
+            )
+        }
+
+        @Test
+        fun `should round trip from and to long`() {
+            for (n in listOf(
+                10L,
+                1L,
+                0L,
+                -1L
+            )) assertEquals(
+                n,
+                n.toBigRational().toLong(),
+                "EQ? long <-> rational -> $n"
+            )
+        }
+
+        @Test
+        fun `should round trip from and to int`() {
+            for (n in listOf(
+                10,
+                1,
+                0,
+                -1
+            )) assertEquals(
+                n,
+                n.toBigRational().toInt(),
+                "EQ? int <-> rational -> $n"
+            )
+        }
+
+        @Test
+        fun `should not convert to short`() {
+            assertThrows<UnsupportedOperationException> {
+                ONE.toShort()
+            }
+        }
+
+        @Test
+        fun `should not convert to char`() {
+            assertThrows<UnsupportedOperationException> {
+                ONE.toChar()
+            }
+        }
+
+        @Test
+        fun `should not convert to byte`() {
+            assertThrows<UnsupportedOperationException> {
+                ONE.toByte()
+            }
+        }
     }
 
-    @Test
-    fun `should add`() {
-        assertEquals(19 over 15, (2 over 3) + (3 over 5))
+    @Nested
+    inner class FidelityToPrimities {
+        @Test
+        fun `should compare as primitives do`() {
+            listOf(
+                0.0 to ZERO,
+                +1.0 to +ONE,
+                -1.0 to -ONE,
+                Double.POSITIVE_INFINITY to POSITIVE_INFINITY,
+                Double.NEGATIVE_INFINITY to NEGATIVE_INFINITY,
+                Double.NaN to NaN
+            ).cartesian().forEach { (a, b) ->
+                assertEquals(
+                    a.primitive == b.primitive,
+                    a.rational == b.rational,
+                    "EQ? -> ${a.rational} == ${b.rational}"
+                )
+                assertEquals(
+                    a.primitive > b.primitive,
+                    a.rational > b.rational,
+                    "GT? -> ${a.rational} > ${b.rational}"
+                )
+                assertEquals(
+                    a.primitive < b.primitive,
+                    a.rational < b.rational,
+                    "LT? -> ${a.rational} < ${b.rational}"
+                )
+            }
+        }
+
+        @Test
+        fun `should treat negation of non-finite values as primitive do`() {
+            assertEquals(NEGATIVE_INFINITY, -POSITIVE_INFINITY)
+            assertEquals(POSITIVE_INFINITY, -NEGATIVE_INFINITY)
+            assertNotEquals(-NaN, -NaN)
+        }
+
+        @Test
+        fun `should print as primitives do`() {
+            assertEquals("${Double.POSITIVE_INFINITY}", "$POSITIVE_INFINITY")
+            assertEquals("${Double.NEGATIVE_INFINITY}", "$NEGATIVE_INFINITY")
+            assertEquals("${Double.NaN}", "$NaN")
+        }
     }
 
-    @Test
-    fun `should subtract`() {
-        assertEquals(1 over 15, (2 over 3) - (3 over 5))
+    @Nested
+    inner class Stringifying {
+        @Test
+        fun `should print nicely`() {
+            assertEquals("0", "$ZERO")
+            assertEquals("1", "$ONE")
+            assertEquals("-1", "${-ONE}")
+            assertEquals("1/10", "${1 over 10}")
+            assertEquals("-1/10", "${-1 over 10}")
+        }
     }
 
-    @Test
-    fun `should multiply`() {
-        assertEquals(6 over 15, (2 over 3) * (3 over 5))
-    }
+    @Nested
+    inner class Arithmetic {
+        @Test
+        fun `should add`() {
+            assertEquals(19 over 15, (2 over 3) + (3 over 5))
+        }
 
-    @Test
-    fun `should divide`() {
-        assertEquals(10 over 9, (2 over 3) / (3 over 5))
-    }
+        @Test
+        fun `should subtract`() {
+            assertEquals(1 over 15, (2 over 3) - (3 over 5))
+        }
 
-    @Test
-    fun `should have no remainder as division is exact`() {
-        assertEquals(ZERO, (2 over 3) % (3 over 5))
-    }
+        @Test
+        fun `should multiply`() {
+            assertEquals(6 over 15, (2 over 3) * (3 over 5))
+        }
 
-    @Test
-    fun `should treat negation of non-finite values as primitive do`() {
-        assertEquals(NEGATIVE_INFINITY, -POSITIVE_INFINITY)
-        assertEquals(POSITIVE_INFINITY, -NEGATIVE_INFINITY)
-        assertNotEquals(-NaN, -NaN)
+        @Test
+        fun `should divide`() {
+            assertEquals(10 over 9, (2 over 3) / (3 over 5))
+        }
+
+        @Test
+        fun `should have no remainder as division is exact`() {
+            assertEquals(ZERO, (2 over 3) % (3 over 5))
+        }
     }
 }
 
