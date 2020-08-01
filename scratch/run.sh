4cf2ceabae6d4184044324ec52109c635a00802d
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-08-01 06:06:36 -0500

    Teach run script about Kotlin classname mangling

--- scratch/run.sh
+++ scratch/run.sh
@@ -4,9 +4,24 @@ jar=target/scratch-0-SNAPSHOT-jar-with-dependencies.jar
 
 test -r $jar || ./mvnw -C package
 
+function mangle-classname() {
+    local name="$1"
+    case $name in
+    *-*) ;;
+    *)
+        echo "${name}Kt"
+        return
+        ;;
+    esac
+
+    name="${name//-/_}"
+    name=""${name^}
+    echo "${name}Kt"
+}
+
 case $# in
 0) class="-jar $jar" ;;
-1) class="-cp $jar x.scratch.${1^}Kt" ;;
+1) class="-cp $jar x.scratch.$(mangle-classname ${1^})" ;;
 *)
     echo "Usage: $0 [FILE-NAME]" >&2
     exit 2
