52d814afd9390e92019b9d23e5756ea4d63eebfa
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-08-17 06:13:37 -0500

    Additional metadata

--- scratch/src/main/kotlin/x/scratch/default-interface-methods-and-metadata.kt
+++ scratch/src/main/kotlin/x/scratch/default-interface-methods-and-metadata.kt
@@ -32,22 +32,26 @@ private val Metadata.pretty
     packageVersion=$packageName
 }"""
 
-private val Int.extraInt get() = when(this) {
-    0 -> "This is a multi-file class facade or part, compiled with -Xmultifile-parts-inherit"
-    1 -> "This class file is compiled by a pre-release version of Kotlin and is not visible to release versions"
-    2 -> "This class file is a compiled Kotlin script source file (.kts)"
-    3 -> "The metadata of this class file is not supposed to be read by the compiler, whose major.minor version is less than the major.minor version of this metadata (mv)"
-    else -> error("Unknown extraInt")
-}
+private val Int.extraInt
+    get() = when (this) {
+        0 -> "This is a multi-file class facade or part, compiled with -Xmultifile-parts-inherit"
+        1 -> "This class file is compiled by a pre-release version of Kotlin and is not visible to release versions"
+        2 -> "This class file is a compiled Kotlin script source file (.kts)"
+        3 -> "The metadata of this class file is not supposed to be read by the compiler, whose major.minor version is less than the major.minor version of this metadata (mv)"
+        4 -> "This class file is compiled with the new Kotlin compiler backend introduced in Kotlin 1.4"
+        5 -> "If the class file is compiled with the new Kotlin compiler backend, the metadata has been verified by the author and no metadata incompatibility diagnostic should be reported at the call site"
+        else -> error("Unknown extraInt: $this")
+    }
 
-private val Int.kind get() = when(this) {
-    1 -> "Class"
-    2 -> "File"
-    3 -> "Synthetic class"
-    4 -> "Multi-file class facade"
-    5 -> "Multi-file class part"
-    else -> error("Unknown KotlinClassHeader.Kind")
-}
+private val Int.kind
+    get() = when (this) {
+        1 -> "Class"
+        2 -> "File"
+        3 -> "Synthetic class"
+        4 -> "Multi-file class facade"
+        5 -> "Multi-file class part"
+        else -> error("Unknown KotlinClassHeader.Kind")
+    }
 
 private interface BobHead {
     fun sallyForth() = println("Tallyho!")
