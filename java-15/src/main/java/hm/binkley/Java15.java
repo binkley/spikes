165e29daab690e751c20191cab8f13c8b4174708
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-09-20 06:29:25 -0500

    Min/max record example

--- java-15/src/main/java/hm/binkley/Java15.java
+++ java-15/src/main/java/hm/binkley/Java15.java
@@ -1,5 +1,9 @@
 package hm.binkley;
 
+import java.util.Comparator;
+import java.util.List;
+
+import static hm.binkley.MinMax.minMax;
 import static java.lang.System.out;
 
 public class Java15 {
@@ -10,11 +14,30 @@ public class Java15 {
                 Uncle
                 Bob says: %s""".formatted(javaVersion);
         if (x instanceof String msg) out.println(new Fooby<>(3, msg));
+
+        out.println("minMax = " +
+                minMax(List.of(3, 2, 1, 5), Integer::compareTo));
     }
 }
 
 record Fooby<T>(int i, T s) {
     public Fooby {
         if (i < 0 || null == s) throw new IllegalArgumentException();
+    }
+}
+
+record MinMax<T>(T min, T max) {
+    static <T> MinMax<T> minMax(final Iterable<T> iterable,
+            final Comparator<T> comparator) {
+        final var it = iterable.iterator();
+        if (!it.hasNext()) throw new IllegalArgumentException();
+        var min = it.next();
+        var max = min;
+        while (it.hasNext()) {
+            var next = it.next();
+            if (0 > comparator.compare(next, min)) min = next;
+            else if (0 < comparator.compare(next, max)) max = next;
+        }
+        return new MinMax<>(min, max);
     }
 }
