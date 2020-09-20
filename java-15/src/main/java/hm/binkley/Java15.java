39646a6b77cba1285f113042061274d9bbf7fd8b
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-09-20 06:19:45 -0500

    More post-11 features

--- java-15/src/main/java/hm/binkley/Java15.java
+++ java-15/src/main/java/hm/binkley/Java15.java
@@ -5,9 +5,10 @@ import static java.lang.System.out;
 public class Java15 {
     public static void main(String[] args) {
         Object x = 7;
-        if (null != System.getProperty("java.version")) x = """
-                uncle
-                bob""";
+        final var javaVersion = System.getProperty("java.version");
+        if (null != javaVersion) x = """
+                Uncle
+                Bob says: %s""".formatted(javaVersion);
         if (x instanceof String msg) out.println(new Fooby<>(3, msg));
     }
 }
