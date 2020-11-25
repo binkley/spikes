d187bd9d4f401abed386358529f1157d266b8be4
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-11-25 07:47:19 -0600

    Try new Path API

--- scratch/src/main/kotlin/x/scratch/path.kt
+++ scratch/src/main/kotlin/x/scratch/path.kt
@@ -1,8 +1,15 @@
 package x.scratch
 
+import java.nio.file.Path
+import kotlin.io.path.*
 import kotlin.io.path.ExperimentalPathApi
 
 @ExperimentalPathApi
 fun main() {
     println("==PATH")
+
+    val baseDir = Path("/")
+    println("✓ BASE -> $baseDir; exists? ${baseDir.exists()}")
+    val subDir = baseDir / "bob"
+    println("✗ SUB -> $subDir -> ${baseDir.exists()}")
 }
