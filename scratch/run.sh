a7d164548a5bb0a4383421319044bfde9ebb31e6
Brian Oxley <boxley@thoughtworks.com>
2020-12-16 09:23:44 -0600

    Debugging

--- scratch/run.sh
+++ scratch/run.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2214,SC2215
+
+export PS4='+${BASH_SOURCE}:${LINENO}:${FUNCNAME[0]:+${FUNCNAME[0]}():} '
+
+set -e
+set -u
+set -o pipefail
 
 package=x.scratch
 jar=target/scratch-0-SNAPSHOT-jar-with-dependencies.jar
@@ -26,6 +33,18 @@ function rebuild-if-needed() {
     [[ ! -e "$jar" || -n "$(find src -type f -newer "$jar")" ]]
 }
 
+debug=false
+while getopts :d-: opt; do
+    [[ $opt == - ]] && opt=${OPTARG%%=*} OPTARG=${OPTARG#*=}
+    case $opt in
+    d | debug) debug=true ;;
+    *) exit 2 ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+$debug && set -x
+
 case $# in
 0) set - -jar "$jar" ;;
 *)
@@ -34,6 +53,7 @@ case $# in
     set - -cp "$jar" "$(mangle-classname "$package.$class")" "$@"
     ;;
 esac
+$debug && set -x # "set - ..." clears the -x flag
 
 rebuild-if-needed && ./mvnw -C package
 
