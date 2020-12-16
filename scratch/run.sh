149ceace385b6230b12fb913795d59d5f3a38527
Brian Oxley <boxley@thoughtworks.com>
2020-12-16 08:05:10 -0600

    Nicer runner

--- scratch/run.sh
+++ scratch/run.sh
@@ -5,23 +5,27 @@ jar=target/scratch-0-SNAPSHOT-jar-with-dependencies.jar
 test -r $jar || ./mvnw -C package
 
 function mangle-classname() {
-    local name="$1"
-    case $name in
-    *-*) ;;
-    *)
-        echo "${name}Kt"
-        return
-        ;;
-    esac
+    local IFS=.
 
-    name="${name//-/_}"
-    name=""${name^}
-    echo "${name}Kt"
+    local -a parts
+    read -r -a parts <<<"$1"
+    local last="${parts[-1]}"
+
+    case "$last" in
+    *-*|*Kt) ;;
+    *) last="${last}Kt" ;;
+    esac
+    last="${last//-/_}"
+    last=""${last^}
+
+    parts[-1]="$last"
+
+    echo "${parts[*]}"
 }
 
 case $# in
 0) class="-jar $jar" ;;
-1) class="-cp $jar x.scratch.$(mangle-classname ${1^})" ;;
+1) class="-cp $jar x.scratch.$(mangle-classname "$1")" ;;
 *)
     echo "Usage: $0 [FILE-NAME]" >&2
     exit 2
